83dbae3e84042df5ed8639baad0828ac887d8686
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 20:17:39 +0200

    Update game-suitpvp.js

--- plugins/game-suitpvp.js
+++ plugins/game-suitpvp.js
@@ -7,9 +7,9 @@ const handler = async (m, {conn, usedPrefix, text}) => {
   if (Object.values(conn.suit).find((room) => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw '*[❗] ترتيب البطولة قيد التشغيل حالياً*';
   const textquien = `*هل تريد أن تلعب لعبة الحجر ورقة مقص مع شخص آخر؟ استخدم*\n\n*—◉ مثال:*\n${usedPrefix}تحدي @${global.suittag}`;
   if (!m.mentionedJid[0]) return m.reply(textquien, m.chat, {mentions: conn.parseMention(textquien)});
-  if (Object.values(conn.suit).find((room) => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `*[❗] 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙰 𝙻𝙰 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙳𝙴𝚂𝙰𝙵𝙸𝙰𝚁 𝙰𝚄𝙽 𝙴𝚂𝚃𝙰 𝙹𝚄𝙶𝙰𝙽𝙳𝙾 𝙾𝚃𝚁𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰, 𝙴𝚂𝙿𝙴𝚁𝙰 𝙰 𝚀𝚄𝙴 𝚃𝙴𝚁𝙼𝙸𝙽𝙴 𝙳𝙴 𝙹𝚄𝙶𝙰𝚁`;
+  if (Object.values(conn.suit).find((room) => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `*[❗] اللاعب ${m.mentionedJid[0].split`@`[0]} يشارك في بطولة حجر ورقة مقص حاليًا، انضم لبطولة أخرى*`;
   const id = 'suit_' + new Date() * 1;
-  const caption = `🎮 **لعبة - حجر ورقة مقص** 🎮\n\n—◉ ${m.sender.split`@`[0]} يتحدى @${m.mentionedJid[0].split`@`[0]} لدورة حجر ورقة مقص\n◉ ارسل "aceptar" للقبول\n◉ ارسل "rechazar" للرفض\nيجب الرد على الرسالة`;
+  const caption = `🎮 **لعبة - حجر ورقة مقص** 🎮\n\n—◉ ${m.sender.split`@`[0]} يتحدى @${m.mentionedJid[0].split`@`[0]} لدورة حجر ورقة مقص\n◉ ارسل "قبول" للقبول\n◉ ارسل "رفض" للرفض\nيجب الرد على الرسالة`;
   const imgplaygame = `https://www.merca2.es/wp-content/uploads/2020/05/Piedra-papel-o-tijera-0003318_1584-825x259.jpeg`;
   conn.suit[id] = {
     chat: await conn.sendMessage(m.chat, {text: caption}, {mentions: await conn.parseMention(caption)}),
@@ -18,13 +18,13 @@ const handler = async (m, {conn, usedPrefix, text}) => {
     p2: m.mentionedJid[0],
     status: 'wait',
     waktu: setTimeout(() => {
-      if (conn.suit[id]) conn.reply(m.chat, `[ ⏳ ] 𝚃𝙸𝙴𝙼𝙿𝙾 𝙳𝙴 𝙴𝚂𝙿𝙴𝚁𝙰 𝙵𝙸𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙾, 𝙴𝙻 𝙿𝚅𝙿 𝚂𝙴 𝙲𝙰𝙽𝙲𝙴𝙻𝙾 𝙿𝙾𝚁 𝙵𝙰𝙻𝚃𝙰 𝙳𝙴 𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰`, m);
+      if (conn.suit[id]) conn.reply(m.chat, `[ ⏳ ] المنافسة انتهت، لاعب آخر لم يرد`, m);
 
       delete conn.suit[id];
     }, timeout), poin, poin_lose, poin_bot, timeout,
   };
 };
-handler.command = /^pvp|suit(pvp)?$/i;
+handler.command = /^ابدأ|suit(pvp)?$/i;
 handler.group = true;
 handler.game = true;
 export default handler;
