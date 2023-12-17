d242c7904488816056e4cf2bb920b25db4904e6d
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 18:25:23 +0200

    Update game-_suitpvp.js

--- plugins/game-_suitpvp.js
+++ plugins/game-_suitpvp.js
@@ -6,9 +6,9 @@ handler.before = async function(m) {
   if (room) {
     let win = '';
     let tie = false;
-    if (m.sender == room.p2 && /^(acc(ept)?|terima|aceptar|gas|aceptare?|nao|gamau|rechazar|ga(k.)?bisa)/i.test(m.text) && m.isGroup && room.status == 'wait') {
-      if (/^(tolak|gamau|rechazar|ga(k.)?bisa)/i.test(m.text)) {
-        const textno = `*[❗] @${room.p2.split`@`[0]} 𝚁𝙴𝙲𝙷𝙰𝚉𝙾 𝙴𝙻 𝙿𝚅𝙿, 𝙴𝙻 𝙹𝚄𝙴𝙶𝙾 𝚂𝙴 𝙲𝙰𝙽𝙲𝙴𝙻𝙰*`;
+    if (m.sender == room.p2 && /^(acc(ept)?|terima|aceptar|gas|aceptare?|nao|gamau|قبول|ga(k.)?bisa)/i.test(m.text) && m.isGroup && room.status == 'wait') {
+      if (/^(tolak|gamau|رفض|ga(k.)?bisa)/i.test(m.text)) {
+        const textno = `*[❗] @${room.p2.split`@`[0]} رفض اللاعب, الجولة ستلغى*`;
         m.reply(textno, null, {mentions: this.parseMention(textno)});
         delete this.suit[room.id];
         return !0;
@@ -16,27 +16,19 @@ handler.before = async function(m) {
       room.status = 'play';
       room.asal = m.chat;
       clearTimeout(room.waktu);
-      const textplay = `🎮 𝙶𝙰𝙼𝙴𝚂 - 𝙿𝚅𝙿 - 𝙶𝙰𝙼𝙴𝚂 🎮\n\n—◉ 𝙴𝙻 𝙹𝚄𝙴𝙶𝙾 𝙲𝙾𝙼𝙸𝙴𝙽𝚉𝙰, 𝙻𝙰𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂 𝙷𝙰𝙽 𝚂𝙸𝙳𝙾 𝙴𝙽𝚅𝙸𝙰𝙳𝙾𝚂 𝙰 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂 𝙳𝙴 @${room.p.split`@`[0]} 𝚈 @${room.p2.split`@`[0]}\n\n◉ 𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙴𝙽 𝚄𝙽𝙰 𝙾𝙿𝙲𝙸𝙾𝙽 𝙴𝙽 𝚂𝚄𝚂 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂, 𝚁𝙴𝚂𝙿𝙴𝙲𝚃𝙸𝚅𝙰𝙼𝙴𝙽𝚃𝙴\n*◉ 𝙴𝙻𝙴𝙶𝙸𝚁 𝙾𝙿𝙲𝙸𝙾𝙽 𝙴𝙽 wa.me/${conn.user.jid.split`@`[0]}*`;
+      const textplay = `🎮 لعبه حجر ورقه مقص 🎮\n\n—◉ تم قبول التحدي من قبل @${room.p.split`@`[0]} و @${room.p2.split`@`[0]}\n\n◉ اختر الحركة التي تريد القيام بها (حجر، ورق، مقص)\n*◉ أرسل الرد بالحركة التي تختارها*\n*مثال: ورق*`;
       m.reply(textplay, m.chat, {mentions: this.parseMention(textplay)});
-      const comienzop = `𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙴 𝚄𝙽𝙰 𝙳𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂
-piedra
-papel
-tijera\n𝙶𝙰𝙽𝙰𝙳𝙾𝚁 +${room.poin}𝚇𝙿\n𝙿𝙴𝚁𝙳𝙴𝙳𝙾𝚁 ${room.poin_lose}𝚇𝙿\n*responda al mensaje con la opción que desea*
-*ejemplo: papel*`;
-      const comienzop2 = `𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙴 𝚄𝙽𝙰 𝙳𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂
-piedra
-papel
-tijera\n𝙶𝙰𝙽𝙰𝙳𝙾𝚁 +${room.poin}𝚇𝙿\n𝙿𝙴𝚁𝙳𝙴𝙳𝙾𝚁 ${room.poin_lose}𝚇𝙿\n*responda al mensaje con la opción que desea*
-*ejemplo: papel*`;
+      const comienzop = `لأفضل تجربة لعبة\nاختر من بين الحركات التالية:\n\nحجر\nورق\nمقص\n\n*أرسل الحركة التي تريدها*\n*مثال: ورق*`;
+      const comienzop2 = `لأفضل تجربة لعبة\nاختر من بين الحركات التالية:\n\nحجر\nورق\nمقص\n\n*أرسل الحركة التي تريدها*\n*مثال: ورق*`;
 
       if (!room.pilih) this.sendMessage(room.p, {text: comienzop}, {quoted: m});
       if (!room.pilih2) this.sendMessage(room.p2, {text: comienzop2}, {quoted: m});
       room.waktu_milih = setTimeout(() => {
-        const iniciativa = `[❗] 𝙽𝙸𝙽𝙶𝚄𝙽 𝙹𝚄𝙶𝙰𝙳𝙾𝚁 𝚃𝙾𝙼𝙾 𝙻𝙰 𝙸𝙽𝙸𝙲𝙸𝙰𝚃𝙸𝚅𝙰 𝙳𝙴 𝙴𝙼𝙴𝙿𝙴𝚉𝙰𝚁 𝙴𝙻 𝙹𝚄𝙴𝙶𝙾, 𝙴𝙻 𝙿𝚅𝙿 𝚂𝙴 𝙰𝙷 𝙲𝙰𝙽𝙲𝙴𝙻𝙰𝙳𝙾`;
+        const iniciativa = `[❗] لقد انتهى اللاعب من اختيار حركته، تم إلغاء الجولة!!`;
         if (!room.pilih && !room.pilih2) this.sendMessage(m.chat, {text: iniciativa}, {quoted: m});
         else if (!room.pilih || !room.pilih2) {
           win = !room.pilih ? room.p2 : room.p;
-          const textnull = `*[❗] @${(room.pilih ? room.p2 : room.p).split`@`[0]} 𝙽𝙾 𝙴𝙻𝙴𝙶𝙸𝚂𝚃𝙴 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙾𝙿𝙲𝙸𝙾𝙽, 𝙵𝙸𝙽 𝙳𝙴𝙻 𝙿𝚅𝙿*`;
+          const textnull = `*[❗] @${(room.pilih ? room.p2 : room.p).split`@`[0]} لم يختر حركة، اللاعب خسر الجولة*`;
           this.sendMessage(m.chat, {text: textnull}, {quoted: m}, {mentions: this.parseMention(textnull)});
           db.data.users[win == room.p ? room.p : room.p2].exp += room.poin;
           db.data.users[win == room.p ? room.p : room.p2].exp += room.poin_bot;
@@ -48,21 +40,21 @@ tijera\n𝙶𝙰𝙽𝙰𝙳𝙾𝚁 +${room.poin}𝚇𝙿\n𝙿𝙴𝚁𝙳𝙴
     }
     const jwb = m.sender == room.p;
     const jwb2 = m.sender == room.p2;
-    const g = /tijera/i;
-    const b = /piedra/i;
-    const k = /papel/i;
-    const reg = /^(tijera|piedra|papel)/i;
+    const g = /مقص/i;
+    const b = /حجر/i;
+    const k = /ورق/i;
+    const reg = /^(مقص|حجر|ورق)/i;
     if (jwb && reg.test(m.text) && !room.pilih && !m.isGroup) {
       room.pilih = reg.exec(m.text.toLowerCase())[0];
       room.text = m.text;
-      m.reply(`*[ ✔ ] 𝙷𝙰𝚂 𝙴𝙻𝙴𝙶𝙸𝙳𝙾 ${m.text}, 𝚁𝙴𝙶𝚁𝙴𝚂𝙰 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾 𝚈 ${room.pilih2 ? `𝚁𝙴𝚅𝙸𝚂𝙰 𝙻𝙾𝚂 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*` : '𝙴𝚂𝙿𝙴𝚁𝙰 𝙻𝙾𝚂 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*'}`);
-      if (!room.pilih2) this.reply(room.p2, '*[❗] 𝙴𝙻 𝙾𝙿𝙾𝙽𝙴𝙽𝚃𝙴 𝙰𝙷 𝙴𝙻𝙴𝙶𝙸𝙳𝙾, 𝙴𝚂 𝚃𝚄 𝚃𝚄𝚁𝙽𝙾 𝙳𝙴 𝙴𝙻𝙴𝙶𝙸𝚁!!*', 0);
+      m.reply(`*[ ✔ ] لقد اخترت ${m.text}, انتظر حتى يختار الخصم*`);
+      if (!room.pilih2) this.reply(room.p2, '*[❗] الخصم لم يقم بتحديد حركته بعد، انتظر!*', 0);
     }
     if (jwb2 && reg.test(m.text) && !room.pilih2 && !m.isGroup) {
       room.pilih2 = reg.exec(m.text.toLowerCase())[0];
       room.text2 = m.text;
-      m.reply(`*[ ✔ ] 𝙷𝙰𝚂 𝙴𝙻𝙴𝙶𝙸𝙳𝙾 ${m.text}, 𝚁𝙴𝙶𝚁𝙴𝚂𝙰 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾 𝚈 ${room.pilih ? `𝚁𝙴𝚅𝙸𝚂𝙰 𝙻𝙾𝚂 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*` : '𝙴𝚂𝙿𝙴𝚁𝙰 𝙻𝙾𝚂 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*'}`);
-      if (!room.pilih) this.reply(room.p, '*[❗] 𝙴𝙻 𝙾𝙿𝙾𝙽𝙴𝙽𝚃𝙴 𝙰𝙷 𝙴𝙻𝙴𝙶𝙸𝙳𝙾, 𝙴𝚂 𝚃𝚄 𝚃𝚄𝚁𝙽𝙾 𝙳𝙴 𝙴𝙻𝙴𝙶𝙸𝚁!!*', 0);
+      m.reply(`*[ ✔ ] لقد اخترت ${m.text}, انتظر حتى يختار الخصم*`);
+      if (!room.pilih) this.reply(room.p, '*[❗] الخصم لم يقم بتحديد حركته بعد، انتظر!*', 0);
     }
     const stage = room.pilih;
     const stage2 = room.pilih2;
@@ -76,9 +68,9 @@ tijera\n𝙶𝙰𝙽𝙰𝙳𝙾𝚁 +${room.poin}𝚇𝙿\n𝙿𝙴𝚁𝙳𝙴
       else if (k.test(stage) && g.test(stage2)) win = room.p2;
       else if (stage == stage2) tie = true;
       this.reply(room.asal, `
-*👑 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴𝙻 𝙿𝚅𝙿 👑*${tie ? '\n*—◉ 𝙴𝙼𝙿𝙰𝚃𝙴!!*' : ''}
-*@${room.p.split`@`[0]} (${room.text})* ${tie ? '' : room.p == win ? ` *𝙶𝙰𝙽𝙾 🥳 +${room.poin}XP*` : ` *𝙿𝙴𝚁𝙳𝙸𝙾 🤡 ${room.poin_lose}XP*`}
-*@${room.p2.split`@`[0]} (${room.text2})* ${tie ? '' : room.p2 == win ? ` *𝙶𝙰𝙽𝙾 🥳 +${room.poin}XP*` : ` *𝙿𝙴𝚁𝙳𝙸𝙾 🤡 ${room.poin_lose}XP*`}
+*👑 نتائج جولة لعبة الحجر ورق مقص 👑*${tie ? '\n*—◉ تعادل!!*' : ''}
+*@${room.p.split`@`[0]} (${room.text})* ${tie ? '' : room.p == win ? ` *الفائز 🥳 +${room.poin}XP*` : ` *الخاسر  🤡 ${room.poin_lose}XP*`}
+*@${room.p2.split`@`[0]} (${room.text2})* ${tie ? '' : room.p2 == win ? ` *الفائز 🥳 +${room.poin}XP*` : ` *الخاسر  🤡 ${room.poin_lose}XP*`}
 `.trim(), m, {mentions: [room.p, room.p2]} );
       if (!tie) {
         db.data.users[win == room.p ? room.p : room.p2].exp += room.poin;
