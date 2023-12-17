28714f5b1ffe6b9c407c0f312e46bfb27837dabe
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:17:28 +0200

    Update buscador-wikipedia.js

--- plugins/buscador-wikipedia.js
+++ plugins/buscador-wikipedia.js
@@ -28,15 +28,17 @@ async function wikipedia(querry) {
     return notFond;
   }
 }
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗️𝐈𝐍𝐅𝐎❗️] 𝙴𝚂𝚃𝙰𝚂 𝚄𝚂𝙰𝙽𝙳𝙾 𝙼𝙰𝙻 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾!!*\n*𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾:*\n*${usedPrefix + command} 𝙿𝚊𝚕𝚊𝚋𝚛𝚊 𝚌𝚕𝚊𝚟𝚎 𝚊 𝚋𝚞𝚜𝚌𝚊𝚛*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Estrellas*`;
-  wikipedia(`${text}`).then((res) => {
-    m.reply(`*𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂 𝙻𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙰:*\n\n` + res.result.isi);
-  }).catch(() => {
-    m.reply('*[❗️𝐈𝐍𝐅𝐎❗️] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽, 𝙿𝚁𝚄𝙴𝙱𝙰 𝚀𝚄𝙴 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝚄𝙽𝙰 𝚂𝙾𝙻𝙰 𝙿𝙰𝙻𝙰𝙱𝚁𝙰 𝚈 𝙻𝙾 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*');
-  });
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*[❗️] يجب إدخال اسم الموضوع الذي تريد البحث عنه*`;
+  try {
+    const result = await wikipedia(`${text}`);
+    m.reply(`*معلومات ويكيبيديا حول: ${result.result.title}*\n\n${result.result.content.join('\n\n')}`);
+  } catch {
+    m.reply('*[❗️] خطأ أثناء جلب المعلومات، يُرجى المحاولة مرة أخرى*');
+  }
 };
-handler.help = ['wikipedia'].map((v) => v + ' <apa>');
+
+handler.help = ['wikipedia'].map((v) => v + ' <موضوع>');
 handler.tags = ['internet'];
 handler.command = /^(wiki|wikipedia)$/i;
 export default handler;
