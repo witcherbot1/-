e7b66a8aaeb3ca50e30ed469bb11c57fd5dd2167
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:11:55 +0200

    Update buscador-animeinfo.js

--- plugins/buscador-animeinfo.js
+++ plugins/buscador-animeinfo.js
@@ -2,32 +2,32 @@ import translate from '@vitalets/google-translate-api';
 import {Anime} from '@shineiichijo/marika';
 const client = new Anime();
 const handler = async (m, {conn, text, usedPrefix}) => {
-  if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] INGRESE EL NOMBRE DE ALGUN ANIME A BUSCAR*`);
+  if (!text) return m.reply(`*[❗اشعار❗] أدخل اسم أنمي للبحث عنه*`);
   try {
     const anime = await client.searchAnime(text);
     const result = anime.data[0];
     const resultes = await translate(`${result.background}`, {to: 'es', autoCorrect: true});
     const resultes2 = await translate(`${result.synopsis}`, {to: 'es', autoCorrect: true});
     const AnimeInfo = `
-🎀 • *Título:* ${result.title}
-🎋 • *Formato:* ${result.type}
-📈 • *Estado:* ${result.status.toUpperCase().replace(/\_/g, ' ')}
-🍥 • *Episodios totales:* ${result.episodes}
-🎈 • *Duración: ${result.duration}*
-✨ • *Basado en:* ${result.source.toUpperCase()}
-💫 • *Estrenado:* ${result.aired.from}
-🎗 • *Finalizado:* ${result.aired.to}
-🎐 • *Popularidad:* ${result.popularity}
-🎏 • *Favoritos:* ${result.favorites}
-🎇 • *Clasificación:* ${result.rating}
-🏅 • *Rango:* ${result.rank}
-♦ • *Trailer:* ${result.trailer.url}
-🌐 • *URL:* ${result.url}
-🎆 • *Background:* ${resultes.text}
-❄ • *Ringkasan:* ${resultes2.text}`;
+🎀 • *العنوان:* ${result.title}
+🎋 • *التنسيق:* ${result.type}
+📈 • *الحالة:* ${result.status.toUpperCase().replace(/\_/g, ' ')}
+🍥 • *إجمالي الحلقات:* ${result.episodes}
+🎈 • *المدة: ${result.duration}*
+✨ • *مستوحى من:* ${result.source.toUpperCase()}
+💫 • *تم الإصدار:* ${result.aired.from}
+🎗 • *تم الانتهاء:* ${result.aired.to}
+🎐 • *الشهرة:* ${result.popularity}
+🎏 • *المفضلة:* ${result.favorites}
+🎇 • *التصنيف:* ${result.rating}
+🏅 • *الترتيب:* ${result.rank}
+♦ • *الإعلان:* ${result.trailer.url}
+🌐 • *الرابط:* ${result.url}
+🎆 • *الخلفية:* ${resultes.text}
+❄ • *ملخص:* ${resultes2.text}}`;
     conn.sendFile(m.chat, result.images.jpg.image_url, 'error.jpg', AnimeInfo, m);
   } catch {
-    throw `*[❗] ERROR, INTENTELO DE NUEVO*`;
+    throw `*[❗] خطأ، حاول مرة أخرى*`;
   }
 };
 handler.command = /^(anime|animeinfo)$/i;
