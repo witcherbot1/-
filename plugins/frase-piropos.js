f7a6e6e9937efc11f4871f1f56e33328e6d8a202
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 17:55:51 +0200

    Update frase-piropos.js

--- plugins/frase-piropos.js
+++ plugins/frase-piropos.js
@@ -4,11 +4,34 @@ const handler = async (m, {conn, text}) => {
   m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.piropo)}"*\n*╚═══════════════════════════*`);
 };
 handler.tags = ['frases'];
-handler.command = ['piropo'];
+handler.command = ['غزل'];
 export default handler;
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
 }
 
-global.piropo = ['Me gustaría ser papel para poder envolver ese bombón.', 'Eres como wifi sin contraseña, todo el mundo te busca', 'Quién fuera bus para andar por las curvas de tu corazón.', 'Quiero volar sin alas y salir de este universo, entrar en el tuyo y amarte en silencio.', 'Quisiera ser mantequilla para derretirme en tu arepa.', 'Si la belleza fuera pecado vos ya estarías en el infierno.', 'Me Gustaría Ser Un Gato Para Pasar 7 Vidas A Tu Lado.', 'Robar Está Mal Pero Un Beso De Tu Boca Sí Me Lo Robaría.', 'Qué Hermoso Es El Cielo Cuando Está Claro Pero Más Hermoso Es El Amor Cuando Te Tengo A Mi Lado.', 'Bonita, Camina Por La Sombra, El Sol Derrite Los Chocolates.', 'Si Fuera Un Correo Electrónico Serías Mi Contraseña.', 'Quisiera que fueses monte para darte machete', 'Perdí mi número de teléfono ¿Me das el tuyo?', '¿Cómo te llamas para pedirte de regalo a Santa Claus?', ' En el cielo hay muchas estrellas, pero la más brillante está en la Tierra y eres tú.', '¿Acaba de salir el sol o es la sonrisa que me regalas hoy?', 'No es el ron ni la cerveza, eres tú quien se me ha subido a la cabeza', 'Si hablamos de matemáticas eres la suma de todos mis deseos.', 'Pareces Google porque tienes todo lo que yo busco.', 'Mi café favorito, es el de tus ojos.', 'Quiero ser photoshop para retocarte todo el cuerpo.', 'Quisiera que fueras cereal, para cucharearte en las mañanas.', 'Quien fuera hambre, para darte tres veces al día.'];
+global.piropo = [
+  'أود أن أكون ورقة لألف أيضًا ألفك هذا الحلو.',
+  'أنت مثل واي فاي بدون كلمة مرور، يبحث الجميع عنك.',
+  'من يكون حافلة للتجول في منحنيات قلبك.',
+  'أريد أن أطير بدون أجنحة والخروج من هذا الكون والدخول إلى كونك وأحبك في صمت.',
+  'أود أن أكون زبدة لأن أذوب في رغيف الخبز الخاص بك.',
+  'إذا كانت الجمال جريمة، فإنك بالفعل في الجحيم.',
+  'أود أن أكون قطة لأمضي 7 حياة بجانبك.',
+  'سرقة خطأ ولكن قبلة من فمك سأسرقها.',
+  'كم هو جميل السماء عندما تكون صافية ولكن الحب أجمل عندما أكون معك.',
+  'جميلة، تمشي في الظلام، الشمس تذوب الشوكولاتة.',
+  'إذا كنت بريدًا إلكتروني، ستكونين كلمة مروري.',
+  'أتمنى أن تكون جبلًا حتى يمكنني أن أعطيك ماشيتي.',
+  'فقدت رقم هاتفي، هل يمكنك إعطائي رقمك؟',
+  'ما اسمك لأطلبك كهدية من سانتا كلوز؟',
+  'في السماء هناك العديد من النجوم، ولكن الألمع هو على الأرض وأنت.',
+  'هل توقفت الشمس عندما أعطيت لي ابتسامتك اليوم؟',
+  'ليس الرون ولا البيرة، أنتم من رفع رأسي.',
+  'إذا تحدثنا عن الرياضيات، فأنت مجموع كل رغباتي.',
+  'تبدو كجوجل لأن لديك كل ما أبحث عنه.',
+  'قهوتي المفضلة هي من عيونك.',
+  'أريد أن أكون فوتوشوب لتعديل كل جسدك.',
+  'أود أن تكون حبوب الإفطار حتى يمكنني أن أملأ ملعقتي في الصباح.',
+  'من كان جائعًا لأعطيك ثلاث وجبات في اليوم.'];
