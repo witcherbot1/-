d6d956b698003150ec9d8afff36edd3d1f874e0f
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 16:29:13 +0200

    Update downloader-x_twitter.js

--- plugins/downloader-x_twitter.js
+++ plugins/downloader-x_twitter.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 let enviando = false;
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) throw `*[❗] Ingrese un enlace de X (twitter), ejemplo: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw`;
+if (!text) throw `*[❗] يرجى إدخال رابط من X (Twitter)، مثال: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw`;
 if (enviando) return;
     enviando = true;
 try {
    await conn.sendMessage(m.chat, {text: global.wait}, {quoted: m}); 
    const res = await TwitterDL(text);
  if (res?.result.type == 'video') {
-     const caption = res?.result.caption ? res.result.caption : '*Aquí tiene su imagen*';
+     const caption = res?.result.caption ? res.result.caption : '*هنا لديك فيديو*';
      for (let i = 0; i < res.result.media.length; i++) {
      await conn.sendMessage(m.chat, {video: {url: res.result.media[i].result[0].url}, caption: caption}, {quoted: m});
      };
      enviando = false;
      return;
  } else if (res?.result.type == 'photo') {
-     const caption = res?.result.caption ? res.result.caption : '*Aquí tiene su imagen*';
+     const caption = res?.result.caption ? res.result.caption : '*هنا لديك فيديو*';
      for (let i = 0; i < res.result.media.length; i++) {
      await conn.sendMessage(m.chat, {image: {url: res.result.media[i].url}, caption: caption}, {quoted: m});
      };
@@ -24,11 +24,11 @@ try {
   }
 } catch {
     enviando = false;
-    throw '*[❗] Error, intente mas tarde.*';
+    throw '*[❗] خطأ، يرجى المحاولة مرة أخرى لاحقًا.*';
     return;
   }
 };    
-handler.command = /^((x|xdl|dlx|twdl|tw|twt|twitter)(dl)?)$/i;
+handler.command = /^((x|xdl|dlx|twdl|tw|twt|تويتر)(dl)?)$/i;
 export default handler;
 
 const _twitterapi = (id) => `https://info.tweeload.site/status/${id}.json`;
@@ -43,7 +43,7 @@ const TwitterDL = async (url) => {
       return resolve({
         status: "error",
         message:
-          "There was an error getting twitter id. Make sure your twitter url is correct!",
+          "حدث خطأ أثناء الحصول على معرف Twitter. تأكد من صحة رابط Twitter الخاص بك!",
       });
       const response = await axios.default(_twitterapi(id[1]), {
         method: "GET",
@@ -57,7 +57,7 @@ const TwitterDL = async (url) => {
       if (response.data.code !== 200) {
         return resolve({
           status: "error",
-          message: "An error occurred while sending the request.",
+          message: "حدث خطأ أثناء إرسال الطلب.",
         });
       }
 
