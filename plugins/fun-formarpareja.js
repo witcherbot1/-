8010b9e94e5034e6806bd6c2580cb565674fd430
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 17:59:06 +0200

    Update fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -1,16 +1,16 @@
-const toM = (a) => '@' + a.split('@')[0];
-function handler(m, {groupMetadata}) {
-  const ps = groupMetadata.participants.map((v) => v.id);
-  const a = ps.getRandom();
+let toM = (a) => "@" + a.split("@")[0];
+function handler(m, { groupMetadata }) {
+  let ps = groupMetadata.participants.map((v) => v.id);
+  let a = ps.getRandom();
   let b;
   do b = ps.getRandom();
   while (b === a);
-  m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, null, {
+  m.reply(`*${toM(a)}, تم تشكيل زوجين 💍 مع ${toM(b)}, ابتدءوا قصة حب جديدة 💓*`, null, {
     mentions: [a, b],
   });
 }
-handler.help = ['formarpareja'];
-handler.tags = ['main', 'fun'];
-handler.command = ['formarpareja', 'formarparejas'];
+handler.help = ["formarpareja"];
+handler.tags = ["main", "fun"];
+handler.command = ["زواج", "formarparejas"];
 handler.group = true;
 export default handler;
