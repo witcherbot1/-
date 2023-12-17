318676a172f251367906e55294217d46dc39100e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 20:41:10 +0200

    Update gc-config.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -2,26 +2,26 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   const isClose = { // Switch Case Like :v
     'open': 'not_announcement',
     'close': 'announcement',
-    'abierto': 'not_announcement',
-    'cerrado': 'announcement',
+    'فتح': 'not_announcement',
+    'غلق': 'announcement',
     'abrir': 'not_announcement',
     'cerrar': 'announcement',
   }[(args[0] || '')];
   if (isClose === undefined) {
     throw `
-*[❗] 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙴𝚁𝚁𝙾𝙽𝙴𝙾!!*
+*[❗] معلمات غير صحيحة!!*
 
-*┏━━━❲ ✨𝙴𝙹𝙴𝙼𝙿𝙻𝙾✨ ❳━━━┓* 
-*┠┉↯ ${usedPrefix + command} abrir*
-*┠┉↯ ${usedPrefix + command} cerrar*
+*┏━━━❲ ✨مثال✨ ❳━━━┓* 
+*┠┉↯ ${usedPrefix + command} فتح*
+*┠┉↯ ${usedPrefix + command} غلق*
 `.trim();
   }
   await conn.groupSettingUpdate(m.chat, isClose);
-  {m.reply('*[ ✔ ] 𝙶𝚁𝚄𝙿𝙾 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*');}
+  {m.reply('*[ ✔ ] تم تحديث إعدادات المجموعة بنجاح*');}
 };
-handler.help = ['group open / close', 'grupo abrir / cerrar'];
+handler.help = ['group open / close', 'جروب فتح / غلق'];
 handler.tags = ['group'];
-handler.command = /^(group|grupo)$/i;
+handler.command = /^(group|اعدادات|جروب)$/i;
 handler.admin = true;
 handler.botAdmin = true;
 export default handler;
