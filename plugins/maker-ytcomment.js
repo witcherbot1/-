b03d7543d54ce53fa1c24c6a685eca150cf2346d
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 21:34:10 +0200

    Update maker-ytcomment.js

--- plugins/maker-ytcomment.js
+++ plugins/maker-ytcomment.js
@@ -1,12 +1,12 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw 'No Text';
+  if (!text) throw 'لم يتم تحديد نص';
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender),
-  }), 'error.png', '*¡¡𝙶𝚁𝙰𝙲𝙸𝙰𝚂 𝙿𝙾𝚁 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁!!*', m);
+  }), 'error.png', '*¡¡صنع تعليق على اليوتيوب!!*', m);
 };
 handler.help = ['ytcomment <comment>'];
 handler.tags = ['maker'];
-handler.command = /^(ytcomment)$/i;
+handler.command = /^(تعليق)$/i;
 export default handler;
