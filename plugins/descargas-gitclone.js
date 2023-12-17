fb3459894eb42bbfc15bf92346a994d1b2147d7c
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 15:10:14 +0200

    Update descargas-gitclone.js

--- plugins/descargas-gitclone.js
+++ plugins/descargas-gitclone.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
 const handler = async (m, {args, usedPrefix, command}) => {
-  if (!args[0]) throw `*[ ℹ️ ] Ingrese un enlace de GitHub.*\n\n*[ ℹ️ ] Ejemplo:* _${usedPrefix + command} https://github.com/BrunoSobrino/TheMystic-Bot-MD _`;
-  if (!regex.test(args[0])) throw '*[ ℹ️ ] El enlace que proporcionó es incorrecto.*';
+  if (!args[0]) throw `*[ ℹ️ ] يرجى إدخال رابط GitHub.*\n\n*[ ℹ️ ] مثال:* _${usedPrefix + command} https://github.com/BrunoSobrino/TheShadwo-Bot-MD _`;
+  if (!regex.test(args[0])) throw '*[ ℹ️ ] الرابط الذي قدمته غير صحيح.*';
   let [_, user, repo] = args[0].match(regex) || [];
   repo = repo.replace(/.git$/, '');
   const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
   const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  m.reply(`*[ ℹ️ ] Se está enviando el archivo. espere...*\n\n*[ ℹ️ ] Si no se envía, podría ser porque supera el límite de tamaño.*`);
+  m.reply(`*[ ℹ️ ] يتم إرسال الملف. انتظر...*\n\n*[ ℹ️ ] إذا لم يتم إرساله، قد يكون ذلك بسبب تجاوز الحجم المسموح به.*`);
   conn.sendFile(m.chat, url, filename, null, m);
 };
 handler.help = ['gitclone <url>'];
