c67d27865d144c56028ad77c3405ed20ce6a628a
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:19:07 +0200

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,17 +2,17 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '⚠️ *_ما الذي تريد البحث عنه في يوتيوب؟_*';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
     switch (v.type) {
-      case 'video': return `
+      case 'فيديو': return `
 ° *_${v.title}_*
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
+↳ 🫐 *_لينك :_* ${v.url}
+↳ 🕒 *_المده :_* ${v.timestamp}
+↳ 📥 *_تم النشر :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`;
     }
   }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
