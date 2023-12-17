fec5b0ee49290c47c2f8226b4a821907b292992c
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 19:22:35 +0200

    Update game-delttt.js

--- plugins/game-delttt.js
+++ plugins/game-delttt.js
@@ -1,10 +1,10 @@
 import MessageType from '@whiskeysockets/baileys';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender));
-  if (room == undefined) return conn.sendButton(m.chat, '*[❗] 𝙽𝙾 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*', wm, null, [['𝙸𝙽𝙸𝙲𝙸𝙰𝚁 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾', `${usedPrefix}ttt partida nueva`]], m);
+  if (room == undefined) return conn.sendButton(m.chat, '*[❗] ليس لديك أي لعبة اكس او الجارية*', wm, null, [['بدء لعبة جديدة', `${usedPrefix}ttt partida nueva`]], m);
   delete conn.game[room.id];
-  await m.reply('*[ ✔ ] 𝚂𝙴 𝙴𝙻𝙸𝙼𝙸𝙽𝙾 𝙻𝙰 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*');
+  await m.reply('*[ ✔ ] تم حذف لعبة اكس او الخاصة بك في الغرفة*');
 };
-handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i;
+handler.command = /^(حذف|deltt|delxo|deltictactoe)$/i;
 handler.fail = null;
 export default handler;
