8ab04e09c0f702fe4e602ac951068521648bb923
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 12:45:33 +0200

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,32 +1,45 @@
-// manden porno test
 const linkRegex = /https:/i;
-export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const delet = m.key.participant;
-  const bang = m.key.id;
-  const bot = global.db.data.settings[this.user.jid] || {};
-  const user = `@${m.sender.split`@`[0]}`;
-  const isGroupLink = linkRegex.exec(m.text);
-  if (chat.antiLink2 && isGroupLink && !isAdmin) {
-    if (isBotAdmin) {
-      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      const linkThisGroup2 = `https://www.youtube.com/`;
-      const linkThisGroup3 = `https://youtu.be/`;
-      if (m.text.includes(linkThisGroup)) return !0;
-      if (m.text.includes(linkThisGroup2)) return !0;
-      if (m.text.includes(linkThisGroup3)) return !0;
+
+export async function before(m, { conn, isAdmin, isBotAdmin, text }) {
+    if (m.isBaileys && m.fromMe) {
+        return !0;
     }
-    await this.sendMessage(m.chat, {text: `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋 ${user} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*`, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*');
-    if (isBotAdmin && bot.restrict) {
-      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*');
-  }
-  return !0;
+
+    if (!m.isGroup) return !1;
+
+    const chat = global.db.data.chats[m.chat];
+    const delet = m.key.participant;
+    const bang = m.key.id;
+    const bot = global.db.data.settings[this.user.jid] || {};
+    const user = `@${m.sender.split`@`[0]}`;
+    const isGroupLink = linkRegex.exec(m.text);
+
+    if (chat.antiLink2 && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+            const linkThisGroup2 = `https://www.youtube.com/`;
+            const linkThisGroup3 = `https://youtu.be/`;
+
+            if (m.text.includes(linkThisGroup) || m.text.includes(linkThisGroup2) || m.text.includes(linkThisGroup3)) {
+                return !0;
+            }
+        }
+
+        await this.sendMessage(m.chat, {
+            text: `*「 روابط محظورة 」*\n*مرحبًا ${user}، يُمنع وضع روابط المجموعات أو روابط الفيديوهات هنا، الرجاء عدم الإعلان...!!*`,
+            mentions: [m.sender]
+        }, { quoted: m });
+
+        if (!isBotAdmin) return m.reply('*[❗تحذير❗] البوت ليس أدمن، لا يمكن وضع روابط المجموعات أو روابط الفيديوهات هنا*');
+
+        if (isBotAdmin && bot.restrict) {
+            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+            const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+            if (responseb[0].status === '404') return;
+        } else if (!bot.restrict) {
+            return m.reply('*[❗تحذير❗] البروبايت ديل بوت غير مُفعل، لا يمكن وضع روابط المجموعات أو روابط الفيديوهات هنا*');
+        }
+    }
+
+    return !0;
 }
