0f763b55cdadf55c4474855656671e634d4c7434
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 21:18:16 +0200

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,14 +3,14 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
+      title: 'لينك الجروب',
+      body: '𝚃𝚑𝚎Zoro - 𝙱𝚘𝚝',
       previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
+      sourceUrl: `https://solo.to/yosef.zoro`}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
-handler.command = /^link(gro?up)?$/i;
+handler.command = /^لينك?$/i;
 handler.group = true;
 handler.botAdmin = true;
 export default handler;
