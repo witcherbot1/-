f4f68c33a3a95fb5482c630e73d16276edc54142
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 16:09:50 +0200

    Update downloader-playlist.js

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search';
 import fs from 'fs';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] Nombre de la canción faltante, por favor ingrese el comando más el nombre/titulo de una canción*\n\n*—◉ Ejemplo:*\n*${usedPrefix + command} Begin you*`;
+  if (!text) throw `*[❗] اسم الأغنية مفقود، يرجى إدخال الأمر متبوعًا بالاسم أو العنوان.*\n\n*—◉ مثال:*\n*${usedPrefix + command} rewrite the stars*`;
   try {
     const vids_ = {
       from: m.sender,
@@ -14,29 +14,29 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
       global.videoList.splice(0, global.videoList.length);
     }
     const results = await yts(text);
-    const textoInfo = `*[❗] Puedes descargar el video que quieras de la siguiente forma:*
-◉ ${usedPrefix}audio <numero>
-◉ ${usedPrefix}video <numero> 
+    const textoInfo = `*[❗] يمكنك تحميل الفيديو الذي تريده بالطريقة التالية:*
+◉ ${usedPrefix}الصوت <numero>
+◉ ${usedPrefix}الفيديو <numero> 
 
-*—◉ Ejemplos:*
-*◉ ${usedPrefix}audio 5*
-*◉ ${usedPrefix}video 8*`.trim();
+*—◉ أمثلة:*
+*◉ ${usedPrefix}صوت 5*
+*◉ ${usedPrefix}فيديو 8*`.trim();
     const teks = results.all.map((v, i) => {
       const link = v.url;
       vids_.urls.push(link);
       return `[${i + 1}] ${v.title}
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_المدة :_* ${v.timestamp}
+↳ 📥 *_تم النشر :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`;
     }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
     conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
     global.videoList.push(vids_);
   } catch {
-    await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
+    await m.reply('*[❗اشعار❗] حدث خطأ، لا يمكن العثور على مصدر الفيديو.*');
   }
 };
 handler.help = ['playlist *<texto>*'];
 handler.tags = ['search'];
-handler.command = /^playlist|playlist2$/i;
+handler.command = /^قائمه-التشغيل|playlist2$/i;
 export default handler;
