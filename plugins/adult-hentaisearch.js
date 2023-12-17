5d313cc19e26fb72fec9266fd1dc53cc2fc2bee3
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 13:52:54 +0200

    Update adult-hentaisearch.js

--- plugins/adult-hentaisearch.js
+++ plugins/adult-hentaisearch.js
@@ -1,40 +1,48 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-const handler = async (m, {conn, text, __dirname, usedPrefix, command}) => {
-  if (!global.db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
-  if (!text) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝙷𝙴𝙽𝚃𝙰𝙸 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!global.db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] التحقق من الفحص للمحتوى البالغين معطل +18، إذا كنت في مجموعة، يرجى تفعيل modohorny*';
+  if (!text) throw '*[❗] يجب إدخال اسم البحث عن الهنتاي المراد، مثال: ' + usedPrefix + command + ' miku*';
+
   const searchResults = await searchHentai(text);
+
   let teks = searchResults.result.map((v, i) => `
-${i+1}. *_${v.title}_*
-↳ 📺 *_Vistas:_* ${v.views}
-↳ 🎞️ *_Link:_* ${v.url}`).join('\n\n');
+${i + 1}. *_${v.title}_*
+↳ 📺 *_المشاهدات:_* ${v.views}
+↳ 🎞️ *_الرابط:_* ${v.url}`).join('\n\n');
+
   let randomThumbnail;
+
   if (searchResults.result.length > 0) {
     const randomIndex = Math.floor(Math.random() * searchResults.result.length);
     randomThumbnail = searchResults.result[randomIndex].thumbnail;
   } else {
     randomThumbnail = 'https://pictures.hentai-foundry.com/e/Error-Dot/577798/Error-Dot-577798-Zero_Two.png';
-    teks = '*[❗] 𝙽𝙾 𝚂𝙴 𝙷𝙰𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙾 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*';
+    teks = '*[❗] لا توجد نتائج للبحث المحدد*';
   }
+
   conn.sendFile(m.chat, randomThumbnail, 'error.jpg', teks, m);
 };
+
 handler.command = /^(hentaisearch|searchhentai)$/i;
 export default handler;
+
 async function searchHentai(search) {
   return new Promise((resolve, reject) => {
-    axios.get('https://hentai.tv/?s=' + search).then(async ({data}) => {
+    axios.get('https://hentai.tv/?s=' + search).then(async ({ data }) => {
       const $ = cheerio.load(data);
       const result = {};
       const res = [];
       result.coder = 'rem-comp';
       result.result = res;
-      result.warning = 'It is strictly forbidden to reupload this code, copyright © 2022 by rem-comp';
-      $('div.flex > div.crsl-slde').each(function(a, b) {
+      result.warning = 'ممنوع بشكل صارم إعادة تحميل هذا الكود، حقوق الطبع © 2022 من قبل rem-comp';
+      $('div.flex > div.crsl-slde').each(function (a, b) {
         const _thumbnail = $(b).find('img').attr('src');
         const _title = $(b).find('a').text().trim();
         const _views = $(b).find('p').text().trim();
         const _url = $(b).find('a').attr('href');
-        const hasil = {thumbnail: _thumbnail, title: _title, views: _views, url: _url};
+        const hasil = { thumbnail: _thumbnail, title: _title, views: _views, url: _url };
         res.push(hasil);
       });
       resolve(result);
