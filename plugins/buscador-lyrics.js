0054eb51071625c963d1a65d418f92f2505eac37
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:15:16 +0200

    Update buscador-lyrics.js

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -1,15 +1,19 @@
-import {find_lyrics} from '@brandond/findthelyrics';
-import {getTracks} from '@green-code/music-track-data';
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
+import { find_lyrics } from '@brandond/findthelyrics';
+import { getTracks } from '@green-code/music-track-data';
+import { googleImage } from '@bochilteam/scraper';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
   const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
-  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`;
+  
+  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل اسم الأغنية أو فقط استجابة لرسالة الأغنية: ${usedPrefix + command} beret ojala*`;
+
   try {
     const result = await getTracks(teks);
     const lyrics = await find_lyrics(`${result[0].artist} ${result[0].title}`);
-    const res = await fetch(global.API('https://some-random-api.com', '/lyrics', {title: result[0].artist + result[0].title}));
+    const res = await fetch(global.API('https://some-random-api.com', '/lyrics', { title: result[0].artist + result[0].title }));
     const json = await res.json();
     let img;
+
     try {
       img = result.album.artwork;
     } catch {
@@ -20,14 +24,17 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
         img = await bochil.getRandom();
       }
     }
+
     const textoLetra = `🎤 𝚃𝙸𝚃𝚄𝙻𝙾: *${result[0].title || ''}*\n👤 𝙰𝚄𝚃𝙾𝚁: *${result[0].artist || ''}*\n\n📃🎵 𝙻𝙴𝚃𝚁𝙰:\n${lyrics || ''}`;
-    await conn.sendMessage(m.chat, {image: {url: img}, caption: textoLetra}, {quoted: m});
-    await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
+
+    await conn.sendMessage(m.chat, { image: { url: img }, caption: textoLetra }, { quoted: m });
+    await conn.sendMessage(m.chat, { audio: { url: result[0].preview }, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
   } catch {
-    throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
+    throw `*[❗𝐈𝐍𝐅𝐎❗] حدث خطأ، يرجى المحاولة مرة أخرى*`;
   }
 };
-handler.help = ['lirik', 'letra'].map((v) => v + ' <Apa>');
+
+handler.help = ['lirik', 'letra'].map((v) => v + ' <اسم الأغنية>');
 handler.tags = ['internet'];
 handler.command = /^(lirik|lyrics|lyric|letra)$/i;
 export default handler;
