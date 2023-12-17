753c2e49e243a347528a0be4a378f82790e79725
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 18:10:29 +0200

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,30 +1,30 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`;
+  if (!text) throw `*[❗] , هل تريد الدردشة؟ قم بالرد بـ*\n\n*مثال: ${usedPrefix + command} مرحبًا بوت*`;
   try {
-    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=ar');
     const resSimi = await api.json();
     m.reply(resSimi.success);
   } catch {
     try {
-      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-      if (text.includes('hola')) text = text.replace('hola', 'Hello');
-      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+      if (text.includes('اهلا')) text = text.replace('اهلا', 'مرحبا');
+      if (text.includes('اهلا')) text = text.replace('اهلا', 'مرحبا');
+      if (text.includes('اهلا')) text = text.replace('اهلا', 'مرحبا');
       const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
       const resu = await reis.json();
       const nama = m.pushName || '1';
       const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
       const res = await api.json();
-      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q=' + res.cnt);
       const resu2 = await reis2.json();
       m.reply(resu2[0][0][0]);
     } catch {
-      throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙴 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
+      throw `*[❗] حدث خطأ، يرجى المحاولة مرة أخرى*`;
     }
   }
 };
 handler.help = ['simi', 'bot'].map((v) => v + ' <teks>');
 handler.tags = ['fun'];
-handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
+handler.command = /^((sim)?simi|bot|سمسم|زورو)$/i;
 export default handler;
