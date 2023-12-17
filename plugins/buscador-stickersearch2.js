00008b44037e6e48553b176eb21d597660cab635
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:16:53 +0200

    Update buscador-stickersearch2.js

--- plugins/buscador-stickersearch2.js
+++ plugins/buscador-stickersearch2.js
@@ -1,20 +1,24 @@
 /* Created By https://github.com/ALBERTO9883 */
 import fs from 'fs';
 import fetch from 'node-fetch';
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {text, usedPrefix, command, conn}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*`;
+import { googleImage } from '@bochilteam/scraper';
+
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+  if (!text) throw `*[❗] يجب إدخال اسم الملصق الذي تريد البحث عنه*`;
   try {
     const res2 = await googleImage(text);
     const sfoto = res2.getRandom();
     const json = await fetch(`https://api.lolhuman.xyz/api/stickerwa?apikey=${lolkeysapi}&query=${text}`);
     const jsons = await json.json();
-    const {stickers} = jsons.result[0];
-    const res = jsons.result.map((v, index) => `🌅 • Resultado: ${1 + index}\n*🥗 • Nombre:* ${v.title}\n*🐢 • Autor:* ${v.author}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`;
+    const { stickers } = jsons.result[0];
+    const res = jsons.result.map((v, index) => 
+      `🌅 • النتيجة: ${1 + index}\n*🥗 • الاسم:* ${v.title}\n*🐢 • الفنان:* ${v.author}\n*🍂 • الرابط:* ${v.url}`
+    ).join`\n\n───\n\n`;
     await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m);
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('*[❗] خطأ، حاول مرة أخرى*');
   }
 };
+
 handler.command = ['stickersearch2', 'searchsticker2', 'stickerssearch2', 'searchstickers2'];
 export default handler;
