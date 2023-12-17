5c4df5487da235dd7efdb1e4e31ef34072619723
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 14:55:27 +0200

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -5,16 +5,16 @@ import fbDownloader from 'fb-downloader-scrapper';
 import {facebook} from '@xct007/frieren-scraper';
 import axios from 'axios';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `*[ ℹ️ ] Ingrese un enlace de Facebook.*\n\n*[ ℹ️ ] Ejemplo:* _${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/ _`;
-  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*[ ℹ️ ] Ingrese un enlace de Facebook.*\n\n*[ ℹ️ ] Ejemplo:* _${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/ _`;
+  if (!args[0]) throw `*[ ℹ️ ] يرجى إدخال رابط من Facebook.*\n\n*[ ℹ️ ] مثال:* _${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/ _`;
+  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*[ ℹ️ ] يرجى إدخال رابط صحيح من Facebook.*\n\n*[ ℹ️ ] مثال:* _${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/ _`;
   try {
-    await m.reply(`*[ ℹ️ ] Se está enviando el video. espere...`);
+    await m.reply(`*[ ℹ️ ] يتم إرسال الفيديو. يرجى الانتظار...`);
     const d2ata = await facebook.v1(args[0]);
     let r2es = '';
     if (d2ata.urls && d2ata.urls.length > 0) {
       r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
     }
-    conn.sendFile(m.chat, r2es, 'error.mp4', `*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._`, m);
+    conn.sendFile(m.chat, r2es, 'error.mp4', `*[ 📥 ] تنزيل - فيس بوك*\n_---> The Mystic Bot._`, m);
   } catch (err1) {
     try {
       const req = await igeh(args[0]);
@@ -25,31 +25,31 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
         const Jjson = await Rres.json();
         let VIDEO = Jjson.result[0];
         if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
-        conn.sendFile(m.chat, VIDEO, 'error.mp4', `*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._`, m);
+        conn.sendFile(m.chat, VIDEO, 'error.mp4', `*[ 📥 ] تنزيل - فيس بوك*\n_---> The Mystic Bot._`, m);
       } catch (err2) {
         try {
           const ress = await fg.fbdl(args[0]);
           const urll = await ress.data[0].url;
-          await conn.sendFile(m.chat, urll, 'error.mp4', '*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._', m);
+          await conn.sendFile(m.chat, urll, 'error.mp4', '*[ 📥 ] تنزيل - فيس بوك*\n_---> The Mystic Bot._', m);
         } catch (err3) {
           try {
             const res = await fbDownloader(args[0]);
             for (const result of res.download) {
               const ur = result.url;
-              await conn.sendFile(m.chat, ur, 'error.mp4', '*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._', m);
+              await conn.sendFile(m.chat, ur, 'error.mp4', '*[ 📥 ] تنزيل - فيس بوك*\n_---> The Mystic Bot._', m);
             }
           } catch (err4) {
             try {
               const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
               const json = await res3.json();
               const url3 = await json.video;
-              await conn.sendFile(m.chat, url3, 'error.mp4', '*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._', m);
+              await conn.sendFile(m.chat, url3, 'error.mp4', '*[ 📥 ] تنزيل - فيس بوك*\n_---> The Mystic Bot._', m);
             } catch (err5) {
               try {
                 const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._', m);
+                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*[ 📥 ] تنزيل - فيس بوك*\n_---> The Zoro Bot._', m);
               } catch (err6) {
-                throw `*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
+                throw `*[ ℹ️ ] حدث خطأ. يرجى المحاولة مرة أخرى في وقت لاحق.*`;
               }
             }
           }
@@ -58,7 +58,7 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
     }
   }
 };
-handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
+handler.command = /^(فيس|fb|فيسبوك|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
 export default handler;
 
 async function igeh(url_media) {
