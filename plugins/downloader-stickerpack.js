771760fe9eb2ffb40a863af0d633fcbd4d81c7b2
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-17 16:19:20 +0200

    Update downloader-stickerpack.js

--- plugins/downloader-stickerpack.js
+++ plugins/downloader-stickerpack.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch';
 import {sticker} from '../lib/sticker.js';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`;
+  if (!text) throw `*[❗] يرجى إدخال رابط حزمة الملصقات ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`;
   try {
     const url = text;
     const res = await fetch(`https://api.akuari.my.id/downloader/stickerpack?link=${url}`);
@@ -13,9 +13,9 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
       // await delay(1500)
     }
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('*[❗] حدث خطأ، لا يمكن العثور على حزمة الملصقات*');
   }
 };
-handler.command = /^stickerpack$/i;
+handler.command = /^تنزيل-ستيكرات/i;
 export default handler;
 // const delay = time => new Promise(res => setTimeout(res, time))
