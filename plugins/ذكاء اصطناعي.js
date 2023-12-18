c3448c467eb847b2e023b4c3a8079b6b7c08838b
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-18 02:32:37 +0200

    Update ذكاء اصطناعي.js

--- plugins/ذكاء اصطناعي.js
+++ plugins/ذكاء اصطناعي.js
@@ -116,5 +116,5 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   }}
  }}
 };
-handler.command = /^(زورو|بوت|ia|robot|openai2|chatgpt2|ia2|robot2|Mystic|MysticBot)$/i;
+handler.command = /^(زورو|بتبوت|ia|robot|openai2|chatgpt2|ia2|robot2|Mystic|MysticBot)$/i;
 export default handler;
