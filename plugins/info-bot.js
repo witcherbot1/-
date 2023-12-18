7f2923c17663da714b357026bef4d46e08709d93
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-18 18:53:54 +0200

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,17 +1,13 @@
-import fs from 'fs';
-const handler = (m) => m;
-handler.all = async function(m) {
-  const vn = './media/bot.mp3';
-  const chat = global.db.data.chats[m.chat];
-  //const s = seconds: '4556'
-  // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-  // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
-  if (/^bot$/i.test(m.text) && !chat.isBanned) {
-    conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا زورو?*`);
-    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-    // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
-  }
-  return !0;
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*هاا مـاذا تريـد/ي* ❓`;
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/c06dce68aa5c883e358b1.jpg', 'image.jpg', message, m);
 };
+
+handler.customPrefix = /^(|bot|بوت)$/i;
+handler.command = new RegExp;
+
 export default handler;
