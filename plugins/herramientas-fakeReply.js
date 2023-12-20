e4dcfc595a84c2b773d3483222197be6a8217968
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 03:33:41 +0200

    Update herramientas-fakeReply.js

--- plugins/herramientas-fakeReply.js
+++ plugins/herramientas-fakeReply.js
@@ -1,12 +1,12 @@
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
+  if (!text) return m.reply(`*[❗] استخدم الأمر كالتالي*\n\n*${usedPrefix + command}* مرحبًا @${m.sender.split`@`[0]} اهلا`, null, {mentions: [m.sender]});
   const cm = copy(m);
   let who;
   if (text.includes('@0')) who = '0@s.whatsapp.net';
   else if (m.isGroup) who = cm.participant = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
+  if (!who) return m.reply(`*[❗] استخدم الأمر كالتالي*\n\n*${usedPrefix + command}* مرحبًا @${m.sender.split`@`[0]} اهلا`, null, {mentions: [m.sender]});
   cm.key.fromMe = false;
   cm.message[m.mtype] = copy(m.msg);
   const sp = '@' + who.split`@`[0];
@@ -19,7 +19,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 };
 handler.help = ['fake <text> @user <text2>'];
 handler.tags = ['tools'];
-handler.command = /^(fitnah|fakereply|fake)$/;
+handler.command = /^(رد_مزيف|فيك-ربلاي|فيك)$/;
 
 export default handler;
 
