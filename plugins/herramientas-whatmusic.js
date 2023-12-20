73ccd98729e7e381d30aabfe2018b4bdfb1827bd
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 04:06:26 +0200

    Update herramientas-whatmusic.js

--- plugins/herramientas-whatmusic.js
+++ plugins/herramientas-whatmusic.js
@@ -10,7 +10,7 @@ const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
   if (/audio|video/.test(mime)) {
-    if ((q.msg || q).seconds > 20) return m.reply('[❗𝐈𝐍𝐅𝐎❗]\n\nEl archivo que carga es demasiado grande, le sugerimos que corte el archivo grande a un archivo más pequeño, 10-20 segundos Los datos de audio son suficientes para identificar');
+    if ((q.msg || q).seconds > 20) return m.reply('[❗]\n\nالملف الذي تم تحميله كبير جدًا، يُرجى تقطيع الملف الكبير إلى ملف أصغر، 10-20 ثانية من بيانات الصوت كافية للتحديد');
     const media = await q.download();
     const ext = mime.split('/')[1];
     fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media);
@@ -19,17 +19,16 @@ const handler = async (m) => {
     if (code !== 0) throw msg;
     const {title, artists, album, genres, release_date} = res.metadata.music[0];
     const txt = `
-𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰
-
-• 📌 𝚃𝙸𝚃𝚄𝙻𝙾: ${title}
-• 👨‍🎤 𝙰𝚁𝚃𝙸𝚂𝚃𝙰: ${artists !== undefined ? artists.map((v) => v.name).join(', ') : 'No encontrado'}
-• 💾 𝙰𝙻𝙱𝚄𝙼: ${album.name || 'No encontrado'}
-• 🌐 𝙶𝙴𝙽𝙴𝚁𝙾: ${genres !== undefined ? genres.map((v) => v.name).join(', ') : 'No encontrado'}
-• 📆 𝙵𝙴𝙲𝙷𝙰 𝙳𝙴 𝙻𝙰𝙽𝚉𝙰𝙼𝙸𝙴𝙽𝚃𝙾: ${release_date || 'No encontrado'}
+النتيجه
+• 📌 *العنوان*: ${title}
+• 👨‍🎤 المغني: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'لم يتم العثور'}
+• 💾 اسم الاغنيه: ${album.name || 'لم يتم العثور'}
+• 🌐 الجنرال: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'لم يتم العثور'}
+• 📆 تاريخ الإصدار: ${release_date || 'لم يتم العثور'}
 `.trim();
     fs.unlinkSync(`./tmp/${m.sender}.${ext}`);
     m.reply(txt);
-  } else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙰𝚄𝙳𝙸𝙾*';
+  } else throw '*[❗] الرد بالصوت*';
 };
-handler.command = /^quemusica|quemusicaes|whatmusic$/i;
+handler.command = /^quemusica|الاسم|الأسم$/i;
 export default handler;
