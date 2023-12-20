5a385c2bbd53a449adeb5dbe57aa5a4fba2f6099
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 04:16:26 +0200

    Update random-cat.js

--- plugins/random-cat.js
+++ plugins/random-cat.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text}) => {
     const res = await fetch('https://api.thecatapi.com/v1/images/search');
     const img = await res.json();
     const caption = `
-_©The Mystic - Bot_
+_𝑍𝑂𝑅𝑂⚡3𝑀𝐾_
 `.trim();
     conn.sendFile(m.chat, img[0].url, 'cat.jpg', caption, m);
   } catch (e) {
