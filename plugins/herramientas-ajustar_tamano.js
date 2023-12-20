6d6ceae50bc71109a614f5df23bd90d86ad18f57
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 03:05:19 +0200

    Update herramientas-ajustar_tamano.js

--- plugins/herramientas-ajustar_tamano.js
+++ plugins/herramientas-ajustar_tamano.js
@@ -3,20 +3,20 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, command, args, text}) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '⚠️️ Responde a una imagen o video.';
-  if (!text) throw '⚠️️ Ingrese el peso nuevo de la imágen/video.';
-  if (isNaN(text)) throw ' 🔢 sólo números';
-  if (!/image\/(jpe?g|png)|video|document/.test(mime)) throw `⚠️️ Formato no soportado`;
+  if (!mime) throw '❗ الرجاء الرد على صورة أو فيديو.';
+  if (!text) throw '⚠️️ الرجاء إدخال حجم الصورة أو الفيديو الجديد.';
+  if (isNaN(text)) throw ' 🔢 يرجى إدخال أرقام فقط.';
+  if (!/image\/(jpe?g|png)|video|document/.test(mime)) throw `⚠️️ الشكل غير مدعوم.`;
   const img = await q.download();
   const url = await uploadImage(img);
 
   if (/image\/(jpe?g|png)/.test(mime)) {
-    conn.sendMessage(m.chat, {image: {url: url}, caption: `Aqui tienes`, fileLength: `${text}`, mentions: [m.sender]}, {ephemeralExpiration: 24*3600, quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `ها هو`, fileLength: `${text}`, mentions: [m.sender]}, {ephemeralExpiration: 24*3600, quoted: m});
   } else if (/video/.test(mime)) {
-    return conn.sendMessage(m.chat, {video: {url: url}, caption: `Aqui tienes`, fileLength: `${text}`, mentions: [m.sender]}, {ephemeralExpiration: 24*3600, quoted: m});
+    return conn.sendMessage(m.chat, {video: {url: url}, caption: `ها هو`, fileLength: `${text}`, mentions: [m.sender]}, {ephemeralExpiration: 24*3600, quoted: m});
   }
 };
 handler.tags = ['tools'];
 handler.help = ['tamaño <cantidad>'];
-handler.command = /^(length|filelength|edittamaño|totamaño|tamaño)$/i;
+handler.command = /^(تحريرالحجم|طول-الملف|edittamaño|totamaño|tamaño)$/i;
 export default handler;
