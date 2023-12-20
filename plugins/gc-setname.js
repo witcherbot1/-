c2b7d6bbf32f7c75f5a8e154c4bb59dc3179bd8b
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 02:37:09 +0200

    Update gc-setname.js

--- plugins/gc-setname.js
+++ plugins/gc-setname.js
@@ -1,6 +1,6 @@
 import Presence from '@whiskeysockets/baileys';
 const handler = async (m, {conn, args, text}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝚀𝚄𝙴 𝚂𝙴𝙰 𝙴𝙻 𝙽𝚄𝙴𝚅𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*`;
+  if (!text) throw `*[❗اشعار❗] مثال : .تغير-الاسم 𝑍𝑂𝑅𝑂⚡3𝑀𝐾*`;
   try {
     const text = args.join` `;
     if (!args || !args[0]) {
@@ -8,12 +8,12 @@ const handler = async (m, {conn, args, text}) => {
       conn.groupUpdateSubject(m.chat, text);
     }
   } catch (e) {
-    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾 𝙷𝚄𝙱𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟸𝟻 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂*';
+    throw '*[❗اشعار❗] *مثال : .تغيرالاسم 𝑍𝑂𝑅𝑂⚡3𝑀𝐾*';
   }
 };
 handler.help = ['setname <text>'];
 handler.tags = ['group'];
-handler.command = /^(setname)$/i;
+handler.command = /^(تغير-الاسم|تغيرالاسم)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
