4d102297f1195fcbb827a3e8bb7085f13f6c0201
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 03:50:46 +0200

    Update herramientas-qrcode.js

--- plugins/herramientas-qrcode.js
+++ plugins/herramientas-qrcode.js
@@ -1,9 +1,9 @@
 import {toDataURL} from 'qrcode';
 const handler = async (m, {text, conn}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙲𝙾𝙳𝙸𝙶𝙾 𝚀𝚁*`;
+  if (!text) throw `*[❗] حط النص او رقم التلفون او اي حاجه انت عاوز تحولها لباركود (Qr Code)*`;
   conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), {scale: 8}), 'qrcode.png', '¯\\_(ツ)_/¯', m);
 };
 handler.help = ['', 'code'].map((v) => 'qr' + v + ' <teks>');
 handler.tags = ['tools'];
-handler.command = /^qr(code)?$/i;
+handler.command = /^qr(code)?|كود|باركود$/i;
 export default handler;
