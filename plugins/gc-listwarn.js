05e6786e7f3fee217138c2aeb6c018d1818b0b19
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 02:22:48 +0200

    Update gc-listwarn.js

--- plugins/gc-listwarn.js
+++ plugins/gc-listwarn.js
@@ -3,15 +3,15 @@ const handler = async (m, {conn, isOwner}) => {
   const warns = global.db.data.users.warn;
   const user = global.db.data.users;
   const imagewarn = './src/warn.jpg';
-  const caption = `⚠️ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝙳𝙾𝚂\n 
+  const caption = `⚠️قائمة الانذارات\n 
 *╔═══════════════════·•*
-║ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
+║ *المجموع : ${adv.length} المستخدمين* ${adv ? '\n' + adv.map(([jid, user], i) => `
 ║
 ║ 1.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/3)*\n║\n║ - - - - - - - - -`.trim()).join('\n') : ''}
-*╚══════════════════·•*`;
+*╚═══════════════════·•*`;
   await conn.sendMessage(m.chat, {text: caption}, {quoted: m}, {mentions: await conn.parseMention(caption)});
 };
-handler.command = /^(listwarn)$/i;
+handler.command = /^(المخالفين|التحذيرات)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
