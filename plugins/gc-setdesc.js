ccc0158e8b94617d390dd8655a6050afaa72273e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 02:33:51 +0200

    Update gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,10 +1,10 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*');
+  m.reply('*✅ تم تعديل الوصف بنجاح*');
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
-handler.command = /^setdesk|setdesc$/i;
+handler.command = /^تغيرالوصف|تغير-الوصف$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
