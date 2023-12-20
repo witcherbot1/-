88e066ab4d8f1f9cc560da111d2a4944642e185b
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 02:17:17 +0200

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -10,7 +10,7 @@ const handler = async (m, {conn, args}) => {
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
-handler.command = /^لينك?$/i;
+handler.command = /^لينك$/i;
 handler.group = true;
 handler.botAdmin = true;
 export default handler;
