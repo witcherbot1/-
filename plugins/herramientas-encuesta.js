87203b7cdaf711b9db1813688c539d5bdf31fc30
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-20 03:30:31 +0200

    Update herramientas-encuesta.js

--- plugins/herramientas-encuesta.js
+++ plugins/herramientas-encuesta.js
@@ -2,9 +2,9 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   let name = await conn.getName(m.sender);
   if (name == 'undefined') name = 'Indefinido';
   const b = text.split('|');
-  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
-  if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
-  const caption = `*ᴇᴄᴜᴇsᴛᴀ ʀᴇᴀʟɪᴢᴀᴅᴀ ᴘᴏʀ:*\n${name}\n*ᴘʀᴇɢᴜɴᴛᴀ:*\n${text.split('|')[0]}`.trim();
+  if (!b[1]) throw `*[❗] صيغة الاستخدام ${usedPrefix + command} سؤال؟ |خيار1|خيار2...*`;
+  if (b[12]) throw `*[❗] صيغة الاستخدام ${usedPrefix + command} سؤال؟ |خيار1|خيار2...*`;
+  const caption = `*استطلاع أجابة بواسطة:*\n${name}\n*السؤال:*\n${text.split('|')[0]}`.trim();
   const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
   const sendPollMessage = {
     messageContextInfo: {
@@ -20,5 +20,5 @@ conn.relayMessage(m.chat, sendPollMessage, {quoted: m});
 };
 handler.help = ['encuesta question|option|option'];
 handler.tags = ['group'];
-handler.command = ['poll', 'encuesta'];
+handler.command = ['استطلاع', 'encuesta'];
 export default handler;
