ed26d03a21dfbb9baf41475d73ea55e99857d0aa
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-21 10:54:14 +0200

    Update مينو زورو.js

--- plugins/مينو زورو.js
+++ plugins/مينو زورو.js
@@ -34,10 +34,12 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850) 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let user = global.db.data.users[m.sender]
 global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const str = `
 ⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢
 *🐉✬⃝╿↵ مرحــبـا ⌊${taguser}⌉*
+┃🐉✬⃝ @${m.sender.split("@")[0]}*
 ── • ◈ • ──
 *🐉✬⃝╿حط قبل كل امر : ⌊ . ⌉*
 *🐉✬⃝╿مـطـوري : +201032389641*
