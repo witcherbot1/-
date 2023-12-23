5ace434cb0d1035ae80251cceff71443d0846a29
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-23 05:20:37 +0200

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -3,7 +3,7 @@ const threshold = 0.72
 let handler = {
 async before(m) {
 let id = m.chat
-if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/*⚡تخمن الشخصيه من الصوت*/i.test(m.quoted.text)) return !0
+if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
 this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
 if (!(id in this.tebaklagu)) return m.reply('انتهت اللعبة')
 if (m.quoted.id == this.tebaklagu[id][0].id) {
