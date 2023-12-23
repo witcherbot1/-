99816816de1b37b0ca5e128956c80b8487b6ef1e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-23 03:08:24 +0200

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,7 +3,7 @@ function handler(m) {
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 handler.alias = ['owner']
-handler.command = /^(owner)$/i
+handler.command = /^(المطور|المالك|مطور)$/i
 handler.help = ['owner']
 handler.tags = ['main']
 export default handler
