c121a3173dfbde5dbc5ca9cc07247207f2a710df
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-24 18:00:35 +0200

    Update herramientas-chatgpt.js

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -116,5 +116,5 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   }}
  }}
 };
-handler.command = /^(زورو|gpt|ia|robot|openai2|chatgpt2|ia2|robot2|Zoro|ZoroBot)$/i;
+handler.command = /^(بوت|gpt|ia|robot|openai2|chatgpt2|ia2|robot2|Zoro|ZoroBot)$/i;
 export default handler;
