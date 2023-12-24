fd77f879196bca8234352ca2bc1576b0ca527d9d
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-24 11:24:52 +0200

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,15 +1,49 @@
-import uploadFile from '../lib/uploadFile.js';
-import uploadImage from '../lib/uploadImage.js';
-const handler = async (m) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*[❗اشعار❗] يجب الرد على صورة أو فيديو لتحويله إلى رابط*';
-  const media = await q.download();
-  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-  const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*تم تحويل الصورة إلى رابط:* ${link}`);
+import uploadtoimgur from '../lib/imgur.js';
+import fs from 'fs';
+import path from 'path';
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  
+  if (!mime) {
+    throw '✳️ Respond to an image/video';
+  }
+  let mediaBuffer = await q.download();
+
+ 
+  if (mediaBuffer.length > 10 * 1024 * 1024) {
+    throw '✴️ Media size exceeds 10 MB. Please upload a smaller file.';
+  }
+
+  let currentModuleDirectory = path.dirname(new URL(import.meta.url).pathname);
+
+  let tmpDir = path.join(currentModuleDirectory, '../tmp');
+  if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir);
+  }
+
+  let mediaPath = path.join(tmpDir, `media_${Date.now()}.${mime.split('/')[1]}`);
+  fs.writeFileSync(mediaPath, mediaBuffer);
+
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+
+  if (isTele) {
+    let link = await uploadtoimgur(mediaPath);
+
+    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2);
+
+    m.reply(`✅ *Media Upload Successful*\n♕ *File Size:* ${fileSizeMB} MB\n♕ *URL:* ${link}`);
+  } else {
+    m.reply(`♕ ${mediaBuffer.length} Byte(s) 
+    ♕ (Unknown)`);
+  }
+
+  fs.unlinkSync(mediaPath);
 };
-handler.help = ['tourl <reply image>'];
-handler.tags = ['sticker'];
-handler.command = /^(تليجراف|تحويل-لرابط)$/i;
+
+handler.help = ['tourl'];
+handler.tags = ['tools'];
+handler.command = ['url', 'tourl'];
+
 export default handler;
