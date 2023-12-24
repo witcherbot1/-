8145f67d02af85f12c3188224b8d54c686fde74b
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-24 11:04:03 +0200

    Update tool-what-image.js

--- plugins/tool-what-image.js
+++ plugins/tool-what-image.js
@@ -1,37 +1,28 @@
-import fs from 'fs'
-import sagiri from 'sagiri'
-
-let sauceClient = sagiri('96a418eb1f0d7581fad16d30e0dbf1dbbdf4d3bd')
-
-let handler = async (m, { conn}) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || ''
-	if (/image/.test(mime)) {
-		let media = Date.now() + '.' + mime.split('/')[1]
-		fs.writeFileSync(media, await q.download())
-		let sauce = await sauceClient(media)
-		let tx = sauce.map(({ url, site, similarity, thumbnail, authorName, authorUrl }) => {
-			return `*❔ Similarity:* ${similarity}%\n*🔎  Site:* ${site}\n*🔗 Url:* ${url}\n*🧧 Thumb:* ${thumbnail}\n*🖌️ Author Name:* ${authorName}\n*✅ Author Url:* ${authorUrl}`
-		}).join('\n\n❑━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━❑\n\n')
-		/*await conn.sendFile(m.chat, sauce[0].thumbnail, 0, txt.trim(), m, false, {thumbnail: Buffer.alloc(0) })
-		fs.unlinkSync(media)*/
-        await conn.reply(m.chat, tx, 0, {
-    contextInfo: { mentionedJid: [m.sender],
-    externalAdReply :{
-    mediaUrl: sauce[0].thumbnail,
-    mediaType: 1,
-    renderLargerThumbnail: true,
-    title: 'KANNA SOURCE IMAGE', 
-    body: null,  
-    sourceUrl: sauce[0].thumbnail, 
-    thumbnail: await (await fetch(sauce[0].thumbnail)).buffer(),
-      }}
-     })
-	} else throw 'Reply imagenya'
-}
-
-handler.help = ['sauce <reply/send image>']
-handler.tags = ['tools']
-handler.command = /^(sauce|source)$/i
-
+/* 
+# Créditos a https://github.com/Undefined17
+•• @Azami19 ••
+*/
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch'
+let handler = async (m) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw `${mg} 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊\n𝙍𝙀𝙎𝙋𝙊𝙉𝘿 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀 𝙊𝙍 𝙑𝙄𝘿𝙀𝙊`
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let caption = `🛑 𝙀𝙉𝙇𝘼𝘾𝙀:\n${link}\n🥏 𝙏𝘼𝙈𝘼𝙉𝙊: ${media.length}\n🚀 𝙀𝙓𝙋𝙄𝙍𝘼𝘾𝙄𝙊𝙉: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 𝘼𝘾𝙊𝙍𝙏𝘼𝘿𝙊: ${await shortUrl(link)}`
+conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
+handler.help = ['tourl']
+handler.tags = ['herramientas']
+handler.command = /^(tourl|upload)$/i
 export default handler
+
+async function shortUrl(url) {
+let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+return await res.text()
+}
