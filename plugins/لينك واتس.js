d909dabae1267ff18aa73e2164fb627e9797a83e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 21:13:47 +0200

    Update لينك واتس.js

--- plugins/لينك واتس.js
+++ plugins/لينك واتس.js
@@ -1,16 +1,17 @@
 let handler = async (m, { conn, usedPrefix, text, command }) => {
   if (!text) throw `Please provide a phone number\n\n📌 Example: *${usedPrefix + command}* 1234567890`;
 
-  const waLink = `https://wa.me/${text}`;
-  const message = `*WhatsApp Link:*\n${waLink}`;
+  const waLink = `Merhaba canım, benim adım Muhammed. WhatsApp destek ekibinde çalışıyorum. WhatsApp'a uyuşturucu satıyorum ve onlar için uyuşturucu satmayı ve uyuşturucu satın almayı teşvik eden reklamlar yayınlıyorum. Artık web sitemiz üzerinden WhatsApp'tan gizlice ilaç satın alabilirsiniz. Veya bu numaradan bize ulaşabilirsiniz 👇
+https://api.whatsapp.com/send?phone=${text}`;
+  const message = `${waLink}`;
 
   conn.sendMessage(m.chat, { text: message, quoted: m, contextInfo: { mentionedJid: [m.sender] } });
 
-  m.react('✅');
+  m.react('☠');
 };
 
 handler.help = ['wa'];
 handler.tags = ['tools'];
-handler.command = ['واتس'];
+handler.command = ['فنش'];
 
 export default handler;
