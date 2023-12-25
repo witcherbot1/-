c25e0c300adbfafc836b1eeb3f552ba2b40a643c
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 11:25:53 +0200

    Update random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -5,6 +5,6 @@ const handler = async (m, {command, conn, usedPrefix}) => {
   conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m);
 // conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
 };
-handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'ايتاتشي', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
+handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'هيناتا', 'inori', 'isuzu', 'ايتاتشي', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'ناروتو', 'نيزوكو', 'sagiri', 'ساسكي', 'sakura', 'cosplay'];
 handler.tags = ['anime'];
 export default handler;
