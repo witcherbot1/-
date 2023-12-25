e4a27d058c162fdf2d20352d68b97d886aac2fa6
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 10:18:24 +0200

    Update rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,21 +1,21 @@
 const xpperlimit = 350;
 const handler = async (m, {conn, command, args}) => {
-  let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  let count = command.replace(/^شراء/i, '');
+  count = count ? /الكل/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count;
     global.db.data.users[m.sender].limit += count;
     conn.reply(m.chat, `
-┌─「 *NOTA DE PAGO* 」
-‣ *Compra nominal* : + ${count}💎 
-‣ *Gastado* : -${xpperlimit * count} XP
+┌─「 *إيصال الشراء* 」
+‣ *الشراء الفعلي* : + ${count}💎 
+‣ *المبلغ المستنفد* : -${xpperlimit * count} XP
 └──────────────`, m);
-  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m);
+  } else conn.reply(m.chat, `❎ عذرًا، ليس لديك ما يكفي من *XP* لشراء *${count}* 💎`, m);
 };
 handler.help = ['Buy', 'Buyall'];
 handler.tags = ['xp'];
-handler.command = ['buy', 'buyall'];
+handler.command = ['شراء', 'شراءالكل'];
 
 handler.disabled = false;
 
