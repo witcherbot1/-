d00bc8202e2e680394666198cfbbbfd2b25863d4
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 10:23:13 +0200

    Update rpg-rob.js

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -1,23 +1,23 @@
 const ro = 3000;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob + 7200000;
-  if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️¡Hey! Espera ${msToTime(time - new Date())} para volver a robar*`;
+  if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️ ${msToTime(time - new Date())} تقدر تسرق تاني بعد*`;
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
-  if (!who) throw `*[❗] Etiqueta a alguien para robar.*`;
-  if (!(who in global.db.data.users)) throw `*[❗] El usuario no se encuentra en mi base de datos.*`;
+  if (!who) throw `*[❗] اعمل منشن لل هتسرقه هه.*`;
+  if (!(who in global.db.data.users)) throw `*[❗] المستخدم غير موجود في قاعدة البيانات الخاصة بي.*`;
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.exp < rob) return m.reply(`😔 @${who.split`@`[0]} tiene menos de *${ro} xp*\nNo robes a un pobre v":`, null, {mentions: [who]});
+  if (users.exp < rob) return m.reply(`😔 @${who.split`@`[0]} لديه أقل من *${ro} xp*\nمتسرقش راجل غلبان":`, null, {mentions: [who]});
   global.db.data.users[m.sender].exp += rob;
   global.db.data.users[who].exp -= rob;
-  m.reply(`*‣ Robaste ${rob} XP a @${who.split`@`[0]}*`, null, {mentions: [who]});
+  m.reply(`*‣ حلالك يحرامي😂 ${rob} XP من @${who.split`@`[0]}*`, null, {mentions: [who]});
   global.db.data.users[m.sender].lastrob = new Date * 1;
 };
 handler.help = ['rob'];
 handler.tags = ['econ'];
-handler.command = ['robar', 'rob'];
+handler.command = ['زرف','اسرق','هجوم'];
 export default handler;
 function msToTime(duration) {
   const milliseconds = parseInt((duration % 1000) / 100);
@@ -27,5 +27,5 @@ function msToTime(duration) {
   hours = (hours < 10) ? '0' + hours : hours;
   minutes = (minutes < 10) ? '0' + minutes : minutes;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
-  return hours + ' Hora(s) ' + minutes + ' Minuto(s)';
+  return hours + ' ساعه(s) ' + minutes + ' دقائق(ثانيه)';
 }
