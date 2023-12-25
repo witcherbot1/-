8e0e08b613debc524c095f1adc369df7ba9cfcb3
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 13:37:04 +0200

    Update random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/YosefZoro1/YosefZoro1/master/src/JSON/anime-${command}.json`)).data;
+  const res = (await axios.get(`https://raw.githubusercontent.com/YosefZoro1/TheMystic-Bot-MD/master/src/JSON/anime-${command}.json`)).data;
   const haha = await res[Math.floor(res.length * Math.random())];
   conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m);
 // conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
 };
-handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'هيناتا', 'inori', 'isuzu', 'ايتاتشي', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'ناروتو', 'نيزوكو', 'sagiri', 'ساسكي', 'sakura', 'cosplay'];
+handler.command = handler.help = ['اكيرا', 'اكياما', 'اناا', 'اسونا', 'أيوزاوا', 'بوروتو', 'تشيهو', 'شيتوجي', 'ديدارا', 'ايرزا', 'الينا', 'ايبا', 'ايميليا', 'هيستيا', 'هيناتا', 'اينوري', 'ايسوزي', 'ايتاتشي', 'ايتوري', 'كاجا', 'كاجيرو', 'كاوري', 'كانيكي', 'كوترو', 'جوزو', 'مادارا', 'ميكاسا', 'ميكو', 'ميناتو', 'ناروتو', 'نيزوكو', 'ساجيري', 'ساسكي', 'ساكورا', 'كوسبلاي'];
 handler.tags = ['anime'];
 export default handler;
