f85df8d1aeeddac570cec6702f49f4be15f8c0b1
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 10:30:57 +0200

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
   let pp = 'https://telegra.ph/file/06cc652844ea19e8aed1c.jpg';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`;
+  if (!(who in global.db.data.users)) throw `المستخدم الذي ذكرته غير مسجل في قاعدة البيانات الخاصة بي`;
   try {
     pp = await conn.profilePictureUrl(who);
   } catch (e) {
@@ -13,18 +13,18 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
     const username = conn.getName(who);
     const prem = global.prems.includes(who.split `@` [0]);
     const sn = createHash('md5').update(who).digest('hex');
-    const str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
-*𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
-*𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+    const str = `*اســمــك:* ${username} ${registered ? '(' + name + ') ': ''}
+*رقـــمــك:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*رابطك:* wa.me/${who.split`@`[0]}${registered ? '\n*العــمر:* ' + age + ' سنه' : ''}
+*الـمــاسـك:* ${limit} 𝚄𝚂𝙾𝚂
+*مــســجــل:* ${registered ? 'نعم': 'لا'}
+*بـريـمــيــوم:* ${premiumTime > 0 ? 'يب' : (isPrems ? 'يب' : 'لا') || ''}
+*الايدي:* 
 ${sn}`;
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
   }
 };
 handler.help = ['profile [@user]'];
 handler.tags = ['xp'];
-handler.command = /^perfil|profile?$/i;
+handler.command = /^بروفايل|بروفايلي?$/i;
 export default handler;
