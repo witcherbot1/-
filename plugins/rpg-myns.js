88338136cabf7401e798a8719914e13844893a5b
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-25 10:03:25 +0200

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -2,12 +2,12 @@ import {createHash} from 'crypto';
 const handler = async function(m, {conn, text, usedPrefix}) {
   const sn = createHash('md5').update(m.sender).digest('hex');
   m.reply(`┏┅ ━━━━━━━━━━━━ ┅ ━
-┃ *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+┃ *الايدي:* 
 ┃ ${sn}
 ┗┅ ━━━━━━━━━━━━ ┅ ━`.trim());
 };
 handler.help = ['myns'];
 handler.tags = ['xp'];
-handler.command = /^(myns|ceksn)$/i;
+handler.command = /^(الايدي|ايدي)$/i;
 handler.register = true;
 export default handler;
