5e477b2387be25b914108bc3f30a1b004f2c72e3
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-26 07:48:14 +0200

    Update تست.js

--- plugins/تست.js
+++ plugins/تست.js
@@ -1,23 +1,10 @@
-import axios from 'axios';
-const handler = async (m, {
-  conn,
-  args,
-  usedPrefix,
-  command,
-}) => {
-  const res = (await axios.get(`https://gist.githubusercontent.com/YosefZoro1/41e80f742a04cbac0b82c838a2f9570d/raw/2d76480d96fc9c90254ae431f2ce005947f9afbb/edit.json`)).data;
-  const mystic = await res[Math.floor(res.length * Math.random())];
-  conn.sendMessage(m.chat, {
-    video: {
-      url: mystic,
-    },
-    caption: `*𝑬𝑫𝑰𝑻 𝑩𝒀 𝒁𝑶𝑹𝑶 ⚡*`,
-  }, {
-    quoted: m,
-  });
-  // conn.sendButton(m.chat, `_Navidad 🧑‍🎄_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
-};
-handler.help = ['navidad'];
-handler.tags = ['internet'];
-handler.command = /^(ديت)$/i;
-export default handler;
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://gist.githubusercontent.com/YosefZoro1/41e80f742a04cbac0b82c838a2f9570d/raw/2d76480d96fc9c90254ae431f2ce005947f9afbb/edit.json`).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `بب`, m)}
+
+handler.help = ['S H A D O W']
+handler.tags = ['S H A D O W']
+handler.command = /^(ايديت)$/i
+export default handler
