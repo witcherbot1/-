d320e046efe5e0ed9e8681258788df8709cce905
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-26 07:28:45 +0200

    Update ايديت مختلف.js

--- plugins/ايديت مختلف.js
+++ plugins/ايديت مختلف.js
@@ -1,10 +1,30 @@
-import axios from 'axios'
-let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://gist.githubusercontent.com/YosefZoro1/41e80f742a04cbac0b82c838a2f9570d/raw/2d76480d96fc9c90254ae431f2ce005947f9afbb/edit.json`)).data  
-let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `𝑬𝑫𝑰𝑻 𝑩𝒀 𝒁𝑶𝑹𝑶 ⚡`, m)}
+import axios from 'axios';
 
-handler.help = ['Z O R O']
-handler.tags = ['Z O R O']
-handler.command = /^(ايديت)$/i
-export default handler
+let handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        // جلب البيانات من الرابط
+        let response = await axios.get('https://gist.githubusercontent.com/YosefZoro1/41e80f742a04cbac0b82c838a2f9570d/raw/2d76480d96fc9c90254ae431f2ce005947f9afbb/edit.json');
+        
+        // التحقق من نجاح الطلب
+        if (response.status !== 200) {
+            throw new Error(`Failed to fetch data. Status code: ${response.status}`);
+        }
+
+        let data = response.data;
+
+        // اختيار فيديو عشوائي من البيانات المستردة
+        let videoUrl = data[Math.floor(Math.random() * data.length)];
+
+        // إرسال الفيديو إلى المجموعة
+        conn.sendFile(m.chat, videoUrl, 'error.mp4', '𝑬𝑫𝑰𝑻 𝑩𝒀 𝒁𝑶𝑹𝑶 ⚡', m);
+    } catch (error) {
+        console.error(error);
+        conn.reply(m.chat, 'حدث خطأ أثناء تنفيذ الأمر. الرجاء المحاولة مرة أخرى لاحقًا.', m);
+    }
+};
+
+handler.help = ['Z O R O'];
+handler.tags = ['Z O R O'];
+handler.command = /^(ايديت)$/i;
+
+export default handler;
