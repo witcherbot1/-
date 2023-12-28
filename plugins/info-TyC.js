f23fbb37c978ed7f9aa045a0d6b8c9e5ad292b3a
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-29 01:08:07 +0200

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -1,56 +1,39 @@
 const handler = async (m, {conn}) => {
   m.reply(global.terminos);
 };
-handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
+handler.customPrefix = /الخصوصيه|سياسه-الخصوصيه|الخصوصية|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
 handler.command = new RegExp;
 export default handler;
 
 global.terminos = `
-*=> El desconocimiento de lo que aqui se menciona no exime al propietario del Bot, Sub Bot o usuario del bot de la sanciones que se puedan derivar❗*
+*=> الجهل بما يتم ذكره هنا لا يعفي مالك البوت أو المستخدم من العقوبات التي قد تنجم عنها! ❗*
 
-*⚠️ Términos de privacidad*
-_1.- La información que reciba el Bot NO se comparte con ningún tercero ni con nadie_
-_2.- Las imágenes/videos/sticker/audios/etc no son compartidos con nadie_
-_3.- Su numero de teléfono no es compartido con absolutamente nadie_
-_4.- Datos de tarjetas, ubicaciones, direcciones, etc son eliminados inmediatamente y no se comparten con absolutamente nadie_
-_5.- Todas las conversaciones son eliminadas cada determinado tiempo y no se hace backup (no se guarda) de ningún tipo de información/conversación_
+*⚠️ سياسة الخصوصية*
+_1.- المعلومات التي يتلقاها البوت لا تتم مشاركتها مع أي جهة ثالثة أو أي شخص_
+_2.- الصور/الفيديوهات/الملصقات/الصوتيات/الخ لا تتم مشاركتها مع أي شخص_
+_3.- رقم هاتفك لا يتم مشاركته مع أي شخص على الإطلاق_
+_4.- يتم حذف بيانات البطاقات، المواقع، العناوين، الخ على الفور ولا تتم مشاركتها مع أي شخص_
+_5.- يتم حذف جميع المحادثات بشكل دوري ولا يتم إجراء نسخ احتياطي (لن يتم الاحتفاظ) بأي نوع من المعلومات/المحادثات_
 
-*⚠️ Términos de uso*
-_1.- No nos hacemos responsables de un mal uso que se le pueda dar al Bot_
-_2.- No nos hacemos responsables de la ignorancia y/o desconocimiento del tema_
-_3.- El Bot no esta activo las 24 horas al menos de que el propietario decida lo contrario_
-_4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial, ya que son personas (terceros) y no tenemos control sobre ell@s_
-_5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
-_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de The Mystic - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
+*⚠️ شروط الاستخدام*
+_1.- لا نتحمل مسؤولية أي استخدام غير صحيح للبوت_
+_2.- لا نتحمل مسؤولية الجهل و/أو عدم معرفة الموضوع_
+_3.- البوت ليس نشطًا على مدار 24 ساعة ما لم يقرر مالك البوت خلاف ذلك_
+_4.- الشركة لا تتحمل مسؤولية استخدام البوت أو البوتات الفرعية غير الرسمية، حيث إنها تتعامل مع أفراد (أطراف ثالثة) وليس لدينا سيطرة عليهم_
+_5.- لا نتحمل مسؤولية الأرقام التي قد تذهب إلى الدعم بسبب استخدام البوت، فإنه دائمًا من المستحسن استخدام أرقام افتراضية لأغراض البوت_
+_6.- الصوتيات/الملاحظات الصوتية/الصور/الفيديوهات أو أي ملف وسائط آخر هي ملك للبوت The Zoro فقط، إذا تم اكتشاف وجودها في بوت آخر، سيتم اتخاذ الإجراءات اللازمة._
 
-*➤ Mensaje del numero del Bot?*
-_- Si en algún momento recibe un mensaje del numero del Bot y no es un comando posiblemente sea de el propietario del Bot o el propietario del numero, este es un persona real por lo que se le pide que no le falte el respeto_
+*➤ رسالة من رقم البوت؟*
+_- إذا تلقيت في أي وقت رسالة من رقم البوت وليست أمرًا ربما تكون من مالك البوت أو مالك الرقم، فهذا شخص حقيقي لذا يُرجى عدم فقدان الاحترام له_
 
-*➤ Base usada para este Bot:*
-*- https://github.com/BochilGaming/games-wabot-md*
+*➤ الشكر ل:*
+*- YOSEF*
 
-*➤ Agradecimientos a:*
-*- Unx21*
-*- Narutomo*
-*- BochilGaming*
-*- Unxteam*
-*- Bochilteam*
-*- ConfuMods*
-*- Theh2so4*
-*- Hiro*
-*- The.sad.boy01*
-*- botstylee*
-*- Amel*
-*- FatihArridho*
-*- Adiwajshing*
-*- Darlyn*
-*- Etc.*
+*⁉️ هل لديك أي أسئلة؟*
+_- إذا كنت لا تزال تواجه أي استفسار أو شكوى بشأن ما تم ذكره هنا أو بخصوص البوت، اتصل بي على رقمي الشخصي الذي سأرد عليه في أقرب وقت ممكن لتقديم حلاً أو لتحمل مسؤولية أي شكوى_
 
-*⁉️ Te quedaron dudas?*
-_- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
+> wa.me/201032389641
+> YOSEF ZORO
+> كل حساباتي: https://solo.to/yosef.zoro
 
-> wa.me/5219992095479
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
-
-*∆ THE MYSTIC - BOT - MD ∆*`;
+*∆ THE ZORO - BOT - MD ∆*`;
