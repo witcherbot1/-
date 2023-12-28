fe87f918c118d43fe4de08cf426e3e65ee84f20a
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-29 01:09:21 +0200

    Update info-covid.js

--- plugins/info-covid.js
+++ plugins/info-covid.js
@@ -1,21 +1,21 @@
 import fetch from 'node-fetch';
 const handler = async (m, {text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝙿𝙰𝙸𝚂, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} Mexico*`;
+  if (!text) throw `*[❗] يرجى إدخال اسم البلد، استخدم ${usedPrefix + command} cairo*`;
   const res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)));
   if (!res.ok) throw await res.text();
   const json = await res.json();
-  if (!json.confirmed) throw 'País?';
+  if (!json.confirmed) throw 'الرجاء تحديد اسم بلد صحيح.';
   if (json.confirmed) {
     m.reply(`
-🌏 País : ${text}
-✅Confirmado : ${json.confirmed.value}
-📉curado : ${json.recovered.value}
-☠️Muertes : ${json.deaths.value}
-💌Info Actualizada : ${json.lastUpdate}
+🌏 البلد: ${text}
+✅ المصابين: ${json.confirmed.value}
+📉 المتعافون: ${json.recovered.value}
+☠️ الوفيات: ${json.deaths.value}
+💌 تحديث: ${json.lastUpdate}
 `.trim());
   } else throw json;
 };
 handler.help = ['covid'].map((v) => v + ' <país>');
 handler.tags = ['info'];
-handler.command = /^(corona|covid|covid19)$/i;
+handler.command = /^(كرونا|كوفد|covid19)$/i;
 export default handler;
