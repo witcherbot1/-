3910bbff0555ddf5eff7e45a944a93979577cd83
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-31 05:36:11 +0200

    Update بايدن.js

--- plugins/بايدن.js
+++ plugins/بايدن.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* زورو معانا+زورو ضايع`
 
   if (!text) throw too
-  let lr = (`https://api.popcat.xyz/biden?text=(text)`)
+  let lr = (`https://api.popcat.xyz/biden?text=${encodeURIComponent(text)}`)
   conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
   𝑍𝑂𝑅𝑂⚡3𝑀𝐾`, m)
 }
