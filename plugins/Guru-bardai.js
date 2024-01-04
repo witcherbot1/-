48701537ed33ef17c6c88c3e348c82d5434ccaf3
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-04 18:27:35 +0200

    Update Guru-bardai.js

--- plugins/Guru-bardai.js
+++ plugins/Guru-bardai.js
@@ -2,42 +2,13 @@ import fetch from 'node-fetch';
 
 var handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `قول اي حاجه!`;
-  }
-
-  if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text;
-  }
-
-  try {
-    m.react(rwait)
-    const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c6e93e154336db7585c98.jpg' },
-      caption: 'سيبني افكر....'
-    }, {quoted: m})
 
   try {
     m.react('⚡');
     var apii = await fetch(`https://aemt.me/bard?text=${text}`);
     var res = await apii.json();
-    await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            imageMessage: { caption: result }
-          }
 
-    await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            imageMessage: { caption: result }
-          }
-        }
-      }, {});
-      m.react(done);
-    }
+    await conn.sendFile(m.chat, 'https://telegra.ph/file/c6e93e154336db7585c98.jpg', 'image.png', res.result, m);
 
   } catch (error) {
     console.error(error);
