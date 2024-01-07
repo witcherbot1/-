85b7796e05f30ec89b0d18b6d432795a752e68bd
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-07 17:59:54 +0200

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,6 +1,4 @@
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-    return true;
-}
     if (m.isBaileys && m.fromMe) return !0;
     if (m.isGroup) return !1;
     if (!m.message) return !0;
