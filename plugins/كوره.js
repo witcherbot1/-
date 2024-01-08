bf9229574a3211972e33d0d5fb85afc5ca2869c6
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-08 11:39:52 +0200

    Update كوره.js

--- plugins/كوره.js
+++ plugins/كوره.js
@@ -7,13 +7,12 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/YosefZoro1/dc532a8c20e1ebe84e6dea44fab78956/raw/dc7969d86cd4d485b26d943920af775364e6e046/game3.json')).json()
+    let src = await (await fetch('https://gist.githubusercontent.com/YosefZoro1/dc532a8c20e1ebe84e6dea44fab78956/raw/2dffa505b68d88a9b36c456e04dfa947a32d34b7/game3.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*· • • ━━ ⌝🐉⌞ ━━ • • ·*
 *${command.toUpperCase()}*
 *🜋↫╎السـؤال ✍🏻⇜『من الاعب ال بالصورة』*
   *🜋↫╎الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)} ┇*
-  *استخدم .انسحب للأنسحاب*
   *🜋↫╎الـجـائزة💰↞ ${poin} نقاط┇*
 ∞┇━━━ •🐉• ━━━┇∞
 *✠ ~تــ✍︎ــوقــيــع ↯:~*
