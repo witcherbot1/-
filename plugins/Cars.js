dba47b14c02d492fa6bd879f1a98eec07bafc181
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-21 17:04:25 +0200

    Create Cars.js

---
+++ plugins/Cars.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+let handler = async (m, { command, conn, usedPrefix }) => {
+  try {
+    let res = (await axios.get('https://api.popcat.xyz/car')).data;
+    
+    if (!res || !res.image || !res.title) {
+      throw new Error('استجابة API غير صحيحة');
+    }
+
+    conn.sendFile(m.chat, res.image, 'car_image.jpg', `${res.title}`, m);
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, 'حدث خطأ أثناء جلب البيانات', m);
+  }
+};
+
+handler.command = handler.help = ['car|c'];
+handler.tags = ['anime'];
+export default handler;
