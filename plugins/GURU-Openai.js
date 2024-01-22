f08125cc53984e1ab6f1ca34362ca7b286dd255e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-22 13:59:46 +0200

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -21,7 +21,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     const model = 'llama';
     const senderNumber = m.sender.replace(/[^0-9]/g, '');
     const session = `GURU_BOT_${senderNumber}`;
-    const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`;
+    const guru2 = `https://aemt.me/bard?text=${prompt}`;
 
     let response = await fetch(guru2);
     let data = await response.json();
