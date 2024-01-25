7c1a509e72b866bb76eec692ff65ce460d624177
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 20:53:25 +0200

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*');
+  m.reply('*[❗تنبيه❗] تم رفع الحظر عن الجروب*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command = /^unbanchat$/i;
+handler.command = /^رفع-الحظر$/i;
 handler.rowner = true;
 export default handler;
