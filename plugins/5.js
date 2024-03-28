fd895cca3857c9637d44f5463dcd5ed7bc983f75
tomioka111 <128500369+tomioka111@users.noreply.github.com>
2024-03-28 22:00:19 +0300

    Update 5.js

--- plugins/5.js
+++ plugins/5.js
@@ -36,7 +36,7 @@ let more = String.fromCharCode(8206)
 let readMore = more.repeat(850) 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-    m.react('📃')
+    m.react('🖇')
     let videoUrl = 'https://telegra.ph/file/847691b0517ba465c3a1b.mp4'
     const str = `
  *⎔ ━─ ─ ╎⊱ ‹🎐› ⊰ ╎─ ─━ ⎔*
