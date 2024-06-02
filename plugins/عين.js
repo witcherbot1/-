5f48ff80e9b46b23cd9ce341d694b23cf9e812d5
tomioka111 <128500369+tomioka111@users.noreply.github.com>
2024-06-02 10:31:50 +0300

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -1,3 +1,5 @@
+
+//صنع بواسطه 👨‍💻♥
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
@@ -7,24 +9,24 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/Hema732828/test11/main/3en.json')).json()
+    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/4e01c190b7d67225ad7a86d388eeedf6/raw/67f0de059cea4b965a3f3bf211c12fc9c48043e5/gistfile1.txt')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*╭━━━[ *${command.toUpperCase()}* ]━━━━⬣
-┃❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
- *لو مش عارف الاجابه قول استخدم.معرفش*
-  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-*╌─━┇『𝚂𝙷𝙸𝙺𝙰ᴮᴼᵀ🐥』┇━─╌*
+    let caption = `*${command.toUpperCase()}*
+  ⎔↞┃الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  *⎔↞┃الـجـائـزة🎖↞ ${poin} نقاط┇*
+┃𝑬𝑹𝑬𝑵_ 𝑩𝑶𝑻┃↞⎔
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇╰━━━〔 *🛡️ 1.4.9* 〕━━━━━⬣`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
-handler.help = ['عين']
+handler.help = ['guessflag']
 handler.tags = ['game']
 handler.command = /^عين/i
 
