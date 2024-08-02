c9049b273ee02fec667ce59180b79126fc2f272f
witcherbot1 <witcherbot11@gmail.com>
2024-08-02 22:50:02 +0300

    3.js

--- plugins/3.js
+++ plugins/3.js
@@ -39,11 +39,11 @@ global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remot
     m.react('🏷')
     let videoUrl = 'https://telegra.ph/file/943cf241bd82ac7e24e18.mp4'
     const str = `
- *⎔ ━─ ─ ╎⊱ ‹🌊› ⊰ ╎─ ─━ ⎔*
+ *⎔ ━─ ─ ╎⊱ ‹🔰› ⊰ ╎─ ─━ ⎔*
 
 *"${taguser} مـرحـبـا"*
 
-*◈┆قائمة اوامر التحويلات┊📜」↶*
+*◈┆ قائمة اوامر التحويلات ┊📜」↶*
 
 *🏷┊↵ملصق」*
 *🏷┊↵ميم」*
@@ -62,8 +62,8 @@ global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remot
 *🏷┊↵انطق2」*
 *🏷┊↵مطلوب」*
 
-◜◈ 𝑩𝑶𝑻 ╎𝑮𝑰𝒀𝑼𝑼 🌊◞
-*⎔ ━─ ─ ╎⊱ ‹🌊› ⊰ ╎─ ─━ ⎔*`
+◜◈ 𝑩𝑶𝑻 ╎ 𝙍𝙊𝘽 🔰◞
+*⎔ ━─ ─ ╎⊱ ‹🔰› ⊰ ╎─ ─━ ⎔*`
     const { result, key, timeout } = await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: str.trim(),  gifPlayback: true,
   gifAttribution: 0}, { quoted: fcontact })
     m.react(done)
