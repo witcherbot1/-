2f06a2d2a2162bd056f0f3acdd238044e6e0e527
witcherbot1 <witcherbot11@gmail.com>
2024-08-03 06:26:53 +0300

    احزر.js

--- plugins/احزر.js
+++ plugins/احزر.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '⚡️┃لم يتم الاجابة علي السؤال بعد┃⚡️ ❯', conn.tebakbendera[id][0])
+        conn.reply(m.chat, '🔰┃لم يتم الاجابة علي السؤال بعد┃🔰 ❯', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/98d564d49cbf9b539fee19f744de7b26/raw/f2a3e68bbcdd2b06f9dbd5f30d70b9fda42fec14/guessflag')).json()
@@ -13,7 +13,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   ⎔↞┃الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)} ┃
   *استخدم .انسحب للأنسحاب*
   ⎔↞┃الـجـائـزة🎖↞ ${poin} نقاط┃
-┃𝙍𝙊𝘽 🔰_ 𝑩𝑶𝑻┃↞⎔
+┃𝙍𝙊𝘽 🔰 𝑩𝑶𝑻┃↞⎔
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
