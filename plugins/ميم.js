8b0a0e84205fcbeaf73950bd760b04bb72ac452c
witcherbot1 <witcherbot11@gmail.com>
2024-08-03 07:06:40 +0300

    ميم.js

--- plugins/ميم.js
+++ plugins/ميم.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   let [a, b] = text.split`+`   
   let lr = (`https://api.popcat.xyz/drake?text1=${a}&text2=${b}`)
   conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
-  𝑍𝑂𝑅𝑂⚡3𝑀𝐾`, m)
+  𝙍𝙊𝘽 🔰3𝑀𝐾`, m)
 }
 handler.help = ['drake']
 handler.tags = ['maker']
