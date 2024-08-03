4728a26070735d893ac300e81278733b00473928
witcherbot1 <witcherbot11@gmail.com>
2024-08-03 06:59:55 +0300

    لو.js

--- plugins/لو.js
+++ plugins/لو.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*╮─━━⊱「‏⤦╿ZORO 𝐁𝐎𝐓╿⤥」⊰━*\n\n*“${pickRandom(global.verdad)}”*\n\n*╯─━━⊱「‏⤣╿ZORO 𝐁𝐎𝐓╿⤤」⊰━*`, m)
+conn.reply(m.chat,`*╮─━━⊱「‏⤦╿𝙍𝙊𝘽 🔰 𝐁𝐎𝐓╿⤥」⊰━*\n\n*“${pickRandom(global.verdad)}”*\n\n*╯─━━⊱「‏⤣╿𝙍𝙊𝘽 🔰 𝐁𝐎𝐓╿⤤」⊰━*`, m)
 }
 handler.help = ['verdad']
 handler.tags = ['fun']
