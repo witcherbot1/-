4f798f282f7bdaa62ae4213ebee8a9ec36e80762
witcherbot1 <witcherbot11@gmail.com>
2024-08-03 06:55:30 +0300

    علم1.js

--- plugins/علم1.js
+++ plugins/علم1.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     }
     let src = await (await fetch('https://raw.githubusercontent.com/ze819/game/master/src/game.js/luffy1.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*❰❖── ~『𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇🇸🇦❯*
+    let caption = `*❰❖── ~『𝙍𝙊𝘽 🔰-𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇🇸🇦❯*
  *•┃❖↞الـوقـت⏳↞* *${(timeout / 1000).toFixed(2)}* *ثانية┇❯*
   
  *•┃❖↞الـجـائـزة💰↞* *${poin}* *نقطه┇❯*
